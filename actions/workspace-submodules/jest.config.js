d68c9cb1f817c80298b47339cb6105b5dd5aeab7
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-03-12 14:04:13 +0900

    test: add Jest and test code

---
+++ actions/workspace-submodules/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  coverageDirectory: 'coverage',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tests/tsconfig.json',
+    },
+  },
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/tests/**/*.ts'],
+};
--- actions/workspace-submodules/src/index.ts
+++ actions/workspace-submodules/src/index.ts
@@ -1,21 +1,11 @@
-import { dirname, relative as relativePath } from 'path';
 import { inspect } from 'util';
 
 import { setFailed, setOutput } from '@actions/core';
-import findGitRoot from 'find-git-root';
-import { getWorkspaces } from 'workspace-tools';
+
+import { getPackageDataList } from './main';
 
 async function main(): Promise<void> {
-    const cwd = process.cwd();
-    const gitRootPath = dirname(findGitRoot(cwd));
-    const workspaceArray = getWorkspaces(cwd);
-    const output = workspaceArray
-        .map(workspaceData => ({
-            'path-git-relative': relativePath(gitRootPath, workspaceData.path) || '.',
-            'package-name': workspaceData.name,
-            'version': workspaceData.packageJson.version ?? null,
-            'is-private': workspaceData.packageJson.private ?? false,
-        }));
+    const output = await getPackageDataList();
     setOutput('result', output);
 }
 
