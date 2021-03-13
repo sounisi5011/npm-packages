17d337e8f8e8470332b179825b792ecc0670b100
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-03-14 04:30:03 +0900

    chore(@sounisi5011/ts-type-util-has-own-property): change the format of comments

--- packages/ts-type-utils/has-own-property/index.test-d.ts
+++ packages/ts-type-utils/has-own-property/index.test-d.ts
@@ -22,12 +22,16 @@ if (hasOwnProp(obj1, 'require')) {
     expectType<number | undefined>(obj1.optional);
     expectType<number | undefined>(obj1.union_undef);
 } else {
-    // Note: The `hasOwnProperty()` method checks that the specified object has its own properties.
-    //       However, it does not check if it has any inherited properties.
-    //       Even if the `hasOwnProperty()` method returns `false`, there is still a possibility that the specified object has the checked property.
+    /**
+     * The `hasOwnProperty()` method checks that the specified object has its own properties.
+     * However, it does not check if it has any inherited properties.
+     * Even if the `hasOwnProperty()` method returns `false`, there is still a possibility that the specified object has the checked property.
+     */
 
-    // TODO: If we found a way to do this, comment out this test:
-    //       How to change `obj1` to any type except `never` in the `else` condition of a type guard
+    /**
+     * @todo If we found a way to do this, comment out this test:
+     *       How to change `obj1` to any type except `never` in the `else` condition of a type guard
+     */
     // expectType<number | undefined>(obj1.require);
 }
 
@@ -37,9 +41,11 @@ if (hasOwnProp(obj1, 'optional')) {
     expectType<number>(obj1.require);
     expectType<number | undefined>(obj1.union_undef);
 } else {
-    // Note: The `hasOwnProperty()` method checks that the specified object has its own properties.
-    //       However, it does not check if it has any inherited properties.
-    //       Even if the `hasOwnProperty()` method returns `false`, there is still a possibility that the specified object has the checked property.
+    /**
+     * The `hasOwnProperty()` method checks that the specified object has its own properties.
+     * However, it does not check if it has any inherited properties.
+     * Even if the `hasOwnProperty()` method returns `false`, there is still a possibility that the specified object has the checked property.
+     */
     expectType<number | undefined>(obj1.optional);
 }
 
@@ -49,12 +55,16 @@ if (hasOwnProp(obj1, 'union_undef')) {
     expectType<number>(obj1.require);
     expectType<number | undefined>(obj1.optional);
 } else {
-    // Note: The `hasOwnProperty()` method checks that the specified object has its own properties.
-    //       However, it does not check if it has any inherited properties.
-    //       Even if the `hasOwnProperty()` method returns `false`, there is still a possibility that the specified object has the checked property.
+    /**
+     * The `hasOwnProperty()` method checks that the specified object has its own properties.
+     * However, it does not check if it has any inherited properties.
+     * Even if the `hasOwnProperty()` method returns `false`, there is still a possibility that the specified object has the checked property.
+     */
 
-    // TODO: If we found a way to do this, comment out this test:
-    //       How to change `obj1` to any type except `never` in the `else` condition of a type guard
+    /**
+     * @todo If we found a way to do this, comment out this test:
+     *       How to change `obj1` to any type except `never` in the `else` condition of a type guard
+     */
     // expectType<number | undefined>(obj1.union_undef);
 }
 
@@ -66,17 +76,21 @@ if (hasOwnProp(obj1, 'nonExistent')) {
     expectType<number | undefined>(obj1.union_undef);
 }
 
+/**
+ * Note: Currently, the `dot-notation` rule for `@typescript-eslint/eslint-plugin@4.17.0` conflicts with TypeScript's `noPropertyAccessFromIndexSignature` option
+ *       see https://github.com/typescript-eslint/typescript-eslint/issues/3104
+ * @todo If `@typescript-eslint/eslint-plugin` supports the `noPropertyAccessFromIndexSignature` option, remove this comment.
+ */
 /* eslint-disable @typescript-eslint/dot-notation */
-// Note: Currently, the `dot-notation` rule for `@typescript-eslint/eslint-plugin@4.17.0` conflicts with TypeScript's `noPropertyAccessFromIndexSignature` option
-//       see https://github.com/typescript-eslint/typescript-eslint/issues/3104
-// TODO: If `@typescript-eslint/eslint-plugin` supports the `noPropertyAccessFromIndexSignature` option, remove this comment.
 
 const obj2: Record<string, boolean> = {};
 
-// Note: Since TypeScript's `noUncheckedIndexedAccess` option is enabled, THIS TEST SHOULD FAIL.
-//       However, `tsd@0.14.0` probably does not support the `noUncheckedIndexedAccess` option.
-// TODO: If `tsd` supports the `noUncheckedIndexedAccess` option, change the tests to the following code:
-//       expectType<boolean | undefined>(obj2['other']);
+/**
+ * Note: Since TypeScript's `noUncheckedIndexedAccess` option is enabled, THIS TEST SHOULD FAIL.
+ *       However, `tsd@0.14.0` probably does not support the `noUncheckedIndexedAccess` option.
+ * @todo If `tsd` supports the `noUncheckedIndexedAccess` option, change the tests to the following code:
+ *       expectType<boolean | undefined>(obj2['other']);
+ */
 expectNotType<boolean | undefined>(obj2['other']);
 expectTypeTscOnly<boolean | undefined>(obj2['other']);
 
@@ -86,9 +100,11 @@ if (hasOwnProp(obj2, 'prop')) {
     expectNotType<boolean | undefined>(obj2['other']);
     expectTypeTscOnly<boolean | undefined>(obj2['other']);
 } else {
-    // Note: The `hasOwnProperty()` method checks that the specified object has its own properties.
-    //       However, it does not check if it has any inherited properties.
-    //       Even if the `hasOwnProperty()` method returns `false`, there is still a possibility that the specified object has the checked property.
+    /**
+     * The `hasOwnProperty()` method checks that the specified object has its own properties.
+     * However, it does not check if it has any inherited properties.
+     * Even if the `hasOwnProperty()` method returns `false`, there is still a possibility that the specified object has the checked property.
+     */
     expectNotType<boolean | undefined>(obj2['prop']);
     expectTypeTscOnly<boolean | undefined>(obj2['prop']);
 
