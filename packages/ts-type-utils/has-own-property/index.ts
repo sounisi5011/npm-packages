8898a0d4ec79d554373dfd15021915aeb466ae2e
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-03-14 01:30:16 +0900

    fix(@sounisi5011/ts-type-util-has-own-property): If optional property is exist, should not contain undefined

--- packages/ts-type-utils/has-own-property/index.ts
+++ packages/ts-type-utils/has-own-property/index.ts
@@ -1,1 +1,8 @@
-export type hasOwnProperty = <T extends PropertyKey>(obj: unknown, prop: T) => obj is Record<T, unknown>;
+export interface hasOwnProperty {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error TS2677: A type predicate's type must be assignable to its parameter's type.
+    //                    Type 'Pick<Required<O>, P>' is not assignable to type 'O'.
+    //                      'Pick<Required<O>, P>' is assignable to the constraint of type 'O', but 'O' could be instantiated with a different subtype of constraint 'unknown'.
+    <O extends unknown, P extends keyof O>(obj: O, prop: P): obj is Pick<Required<O>, P>;
+    <P extends PropertyKey>(obj: unknown, prop: P): obj is Record<P, unknown>;
+}
