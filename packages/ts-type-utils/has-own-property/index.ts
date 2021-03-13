8940d0bb195448d0ef8149b4cdd7763e4387765c
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-03-14 02:31:44 +0900

    fix(@sounisi5011/ts-type-util-has-own-property): object should have properties other than checked ones

--- packages/ts-type-utils/has-own-property/index.ts
+++ packages/ts-type-utils/has-own-property/index.ts
@@ -1,8 +1,4 @@
 export interface hasOwnProperty {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error TS2677: A type predicate's type must be assignable to its parameter's type.
-    //                    Type 'Pick<Required<O>, P>' is not assignable to type 'O'.
-    //                      'Pick<Required<O>, P>' is assignable to the constraint of type 'O', but 'O' could be instantiated with a different subtype of constraint 'unknown'.
-    <O extends unknown, P extends keyof O>(obj: O, prop: P): obj is Pick<Required<O>, P>;
+    <O extends unknown, P extends keyof O>(obj: O, prop: P): obj is O & Pick<Required<O>, P>;
     <P extends PropertyKey>(obj: unknown, prop: P): obj is Record<P, unknown>;
 }
