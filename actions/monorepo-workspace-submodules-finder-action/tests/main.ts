f3290b1a3983a088177d7f9d8aefa4e15def5e91
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-03-14 18:21:51 +0900

    test(monorepo-workspace-submodules-finder-action): fix test code

--- actions/monorepo-workspace-submodules-finder-action/tests/main.ts
+++ actions/monorepo-workspace-submodules-finder-action/tests/main.ts
@@ -1,3 +1,5 @@
+import type { hasOwnProperty } from '@sounisi5011/ts-type-util-has-own-property';
+
 import pkg from '../package.json';
 import { getPackageDataList, PackageData } from '../src/main';
 
@@ -7,7 +9,10 @@ describe('getPackageDataList()', () => {
             'path-git-relative': 'actions/monorepo-workspace-submodules-finder-action',
             'package-name': pkg.name,
             'version': pkg.version ?? null,
-            'is-private': pkg.private ?? false,
+            // eslint-disable-next-line jest/no-if
+            'is-private': (Object.prototype.hasOwnProperty.call as hasOwnProperty)(pkg, 'private')
+                ? Boolean(pkg.private)
+                : false,
         };
         await expect(getPackageDataList()).resolves.toContainEqual(packageData);
     });
