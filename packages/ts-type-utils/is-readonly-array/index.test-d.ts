fe1d65beb1885d0cb39a83213ac4695b7c6995aa
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-04-10 19:44:17 +0900

    test(ts-type-util-is-readonly-array): refactor the tests of type

--- packages/ts-type-utils/is-readonly-array/index.test-d.ts
+++ packages/ts-type-utils/is-readonly-array/index.test-d.ts
@@ -6,54 +6,52 @@ const isArray = Array.isArray as isReadonlyArray;
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
 
-declare const gen: <T>() => T;
+export function test1(param: string | readonly string[]): void {
+    if (isArray(param)) {
+        expectType<readonly string[]>(param);
+    } else {
+        expectType<string>(param);
+    }
 
-const data1 = gen<string | readonly string[]>();
-
-if (isArray(data1)) {
-    expectType<readonly string[]>(data1);
-} else {
-    expectType<string>(data1);
+    if ((Array.isArray as isReadonlyArray)(param)) {
+        expectType<readonly string[]>(param);
+    } else {
+        expectType<string>(param);
+    }
 }
 
-if ((Array.isArray as isReadonlyArray)(data1)) {
-    expectType<readonly string[]>(data1);
-} else {
-    expectType<string>(data1);
+export function test2(param: string | string[]): void {
+    if (isArray(param)) {
+        expectType<string[]>(param);
+    } else {
+        expectType<string>(param);
+    }
+
+    if ((Array.isArray as isReadonlyArray)(param)) {
+        expectType<string[]>(param);
+    } else {
+        expectType<string>(param);
+    }
 }
 
-const data2 = gen<string | string[]>();
+export function test3(param: boolean | string[] | readonly number[]): void {
+    if (isArray(param)) {
+        expectType<string[] | readonly number[]>(param);
+    } else {
+        expectType<boolean>(param);
+    }
 
-if (isArray(data2)) {
-    expectType<string[]>(data2);
-} else {
-    expectType<string>(data2);
+    if ((Array.isArray as isReadonlyArray)(param)) {
+        expectType<string[] | readonly number[]>(param);
+    } else {
+        expectType<boolean>(param);
+    }
 }
 
-if ((Array.isArray as isReadonlyArray)(data2)) {
-    expectType<string[]>(data2);
-} else {
-    expectType<string>(data2);
-}
-
-const data3 = gen<boolean | string[] | readonly number[]>();
-
-if (isArray(data3)) {
-    expectType<string[] | readonly number[]>(data3);
-} else {
-    expectType<boolean>(data3);
-}
-
-if ((Array.isArray as isReadonlyArray)(data3)) {
-    expectType<string[] | readonly number[]>(data3);
-} else {
-    expectType<boolean>(data3);
-}
-
-const data4: unknown = [];
-
-if (isArray(data4)) {
-    expectType<readonly unknown[]>(data4);
-} else {
-    expectType<unknown>(data4);
+export function test4(param: unknown): void {
+    if (isArray(param)) {
+        expectType<readonly unknown[]>(param);
+    } else {
+        expectType<unknown>(param);
+    }
 }
