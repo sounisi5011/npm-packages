458a3103b85585893e035491c411f5642daebda3
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-04-20 21:45:16 +0900

    test(file-encryptor): fix `tests/unit/header.ts`

--- packages/file-encryptor/tests/unit/header.ts
+++ packages/file-encryptor/tests/unit/header.ts
@@ -3,7 +3,14 @@ import * as crypto from 'crypto';
 import * as multicodec from 'multicodec';
 import * as varint from 'varint';
 
-import { createHeader, HeaderDataWithCiphertextLength, parseHeader } from '../../src/header';
+import {
+    createHeader,
+    HeaderDataWithCiphertextLength,
+    parseHeaderData,
+    parseHeaderLength,
+    validateCID,
+} from '../../src/header';
+import { cidByteList } from '../../src/header/content-identifier';
 import { padStartArray } from '../helpers';
 
 const dummyHeaderData: HeaderDataWithCiphertextLength = {
@@ -22,7 +29,7 @@ const dummyHeaderData: HeaderDataWithCiphertextLength = {
     ciphertextLength: 0,
 };
 
-const cidByte = Buffer.from(varint.encode(0x305011));
+const cidByte = Buffer.from(cidByteList);
 const ciphertextLengthByte = Buffer.from(varint.encode(dummyHeaderData.ciphertextLength));
 
 describe('createHeader()', () => {
@@ -69,75 +76,75 @@ describe('createHeader()', () => {
     });
 });
 
-describe('parseHeader()', () => {
-    describe('unknown CID', () => {
-        it.each([
-            ['zero length', Buffer.from([])],
-            ['invalid varint', Buffer.from([0xFF])],
-            ['maximum of 9 bytes', Buffer.from(padStartArray([0x00], 9, 0xFF))],
-        ])('%s', (_, data) => {
-            expect(() => parseHeader(data)).toThrowWithMessage(
-                Error,
-                `Could not decode identifier. Multicodec compliant identifiers are required.`,
-            );
-        });
-        it.each([
-            ['00', 0x00],
-            ['0A', 0x0A],
-            ['10', 0x10],
-            ['0100', 0x0100],
-            ['1000', 0x1000],
-            ['010000', 0x010000],
-            ['100000', 0x100000],
-        ])('no match / 0x%s', (codeStr, codeInt) => {
-            const data = Buffer.from(varint.encode(codeInt));
-            expect(() => parseHeader(data)).toThrowWithMessage(
-                Error,
-                new RegExp(
-                    String.raw`^Invalid identifier detected\.`
-                        + String.raw` The identifier must be 0x[0-9A-F]+, encoded as unsigned varint\.`
-                        + String.raw` Received 0x${codeStr}$`,
-                ),
-            );
-        });
+describe('validateCID()', () => {
+    it.each([
+        ['zero length', Buffer.from([])],
+        ['invalid varint', Buffer.from([0xFF])],
+        ['maximum of 9 bytes', Buffer.from(padStartArray([0x00], 9, 0xFF))],
+    ])('%s', (_, data) => {
+        expect(() => validateCID({ data })).toThrowWithMessage(
+            Error,
+            `Could not decode identifier. Multicodec compliant identifiers are required.`,
+        );
     });
+    it.each([
+        ['00', 0x00],
+        ['0A', 0x0A],
+        ['10', 0x10],
+        ['0100', 0x0100],
+        ['1000', 0x1000],
+        ['010000', 0x010000],
+        ['100000', 0x100000],
+    ])('no match / 0x%s', (codeStr, codeInt) => {
+        const data = Buffer.from(varint.encode(codeInt));
+        expect(() => validateCID({ data })).toThrowWithMessage(
+            Error,
+            new RegExp(
+                String.raw`^Invalid identifier detected\.`
+                    + String.raw` The identifier must be 0x[0-9A-F]+, encoded as unsigned varint\.`
+                    + String.raw` Received 0x${codeStr}$`,
+            ),
+        );
+    });
+});
+
+describe('parseHeaderLength()', () => {
     describe('invalid header byte length', () => {
         it.each([
             ['zero length bytes', Buffer.from([])],
             ['invalid varint', Buffer.from([0xFF])],
             ['maximum of 9 bytes', Buffer.from(padStartArray([0x00], 9, 0xFF))],
         ])('%s', (_, headerLengthBytes) => {
-            const data = Buffer.concat([cidByte, headerLengthBytes]);
-            expect(() => parseHeader(data)).toThrowWithMessage(
+            const data = headerLengthBytes;
+            expect(() => parseHeaderLength({ data })).toThrowWithMessage(
                 Error,
                 `Could not decode header size. The byte length of the header encoded as unsigned varint is required.`,
             );
         });
         it('zero length data', () => {
-            const data = Buffer.from([...cidByte, 0x00]);
-            expect(() => parseHeader(data)).toThrowWithMessage(
+            const data = Buffer.from([0x00]);
+            expect(() => parseHeaderLength({ data })).toThrowWithMessage(
                 Error,
                 `Invalid header byte length received: 0`,
             );
         });
-        describe('invalid length bytes', () => {
-            it.each([
-                [9, 0],
-                [7, 3],
-                [11, 10],
-                [11, 10],
-                [300, 259],
-            ])('need %i bytes / actual %i bytes', (needLen, actualLen) => {
-                const data = Buffer.from([
-                    ...cidByte,
-                    ...varint.encode(needLen),
-                    ...crypto.randomBytes(actualLen),
-                ]);
-                expect(() => parseHeader(data)).toThrowWithMessage(
-                    Error,
-                    `Could not read header table. ${needLen} byte length header is required. Received data: ${actualLen} bytes`,
-                );
-            });
+    });
+});
+
+describe('parseHeaderData()', () => {
+    describe('invalid length bytes', () => {
+        it.each([
+            [9, 0],
+            [7, 3],
+            [11, 10],
+            [11, 10],
+            [300, 259],
+        ])('need %i bytes / actual %i bytes', (needLen, actualLen) => {
+            const data = crypto.randomBytes(actualLen);
+            expect(() => parseHeaderData({ data, headerByteLength: needLen })).toThrowWithMessage(
+                Error,
+                `Could not read header table. ${needLen} byte length header is required. Received data: ${actualLen} bytes`,
+            );
         });
     });
 });
