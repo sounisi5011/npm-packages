ad40522b019b82f8ab434bb1889153fb3f49e2f6
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-05-20 20:20:35 +0900

    feat(stream-transform-from): add `transformFrom()` function and `TransformFromAsyncIterable` class

--- packages/stream-transform-from/src/index.ts
+++ packages/stream-transform-from/src/index.ts
@@ -1,3 +1,5 @@
+import { Transform } from 'stream';
+
 import type * as stream from 'stream';
 
 /**
@@ -19,11 +21,101 @@ type OutputChunkType<T extends stream.TransformOptions> = (
         : string | Buffer | Uint8Array
 );
 
-export function transformFrom<TOpts extends stream.TransformOptions>(
-    transformFn: (
-        source: AsyncIterableIterator<InputChunkType<TOpts>>,
-    ) => Iterable<OutputChunkType<TOpts>> | AsyncIterable<OutputChunkType<TOpts>>,
+type TransformFunction<TOpts extends stream.TransformOptions> = (
+    source: AsyncIterableIterator<InputChunkType<TOpts>>,
+) => Iterable<OutputChunkType<TOpts>> | AsyncIterable<OutputChunkType<TOpts>>;
+
+type InputData<TOpts extends stream.TransformOptions> =
+    | { chunk: InputChunkType<TOpts>; done?: false }
+    | { done: true };
+
+export class TransformFromAsyncIterable<
+    TOpts extends Omit<stream.TransformOptions, 'transform' | 'flush'>
+> extends Transform {
+    private done: stream.TransformCallback | undefined;
+    private transformInput: ((data: InputData<TOpts>) => void) | undefined;
+
+    constructor(
+        private readonly transformFn: TransformFunction<TOpts>,
+        opts?: TOpts,
+    ) {
+        super(opts);
+    }
+
+    _transform(
+        chunk: InputChunkType<TOpts>,
+        _encoding: BufferEncoding,
+        callback: stream.TransformCallback,
+    ): void {
+        this.done = callback;
+        this.setInput({ chunk });
+    }
+
+    _flush(callback: stream.TransformCallback): void {
+        this.done = callback;
+        this.setInput({ done: true });
+    }
+
+    private setInput(inputData: InputData<TOpts>): void {
+        if (this.transformInput) {
+            this.transformInput(inputData);
+            return;
+        }
+
+        const source = this.createSource(inputData);
+        const result = this.transformFn(source);
+        void (async () => {
+            try {
+                for await (const chunk of result) {
+                    this.pushChunk(chunk);
+                }
+            } catch (error) {
+                this.pushError(error);
+            }
+        })();
+    }
+
+    private async *createSource(
+        parentInputData: InputData<TOpts> | undefined,
+    ): AsyncIterableIterator<InputChunkType<TOpts>> {
+        while (true) {
+            const inputPromise = new Promise<InputData<TOpts>>(resolve => {
+                this.transformInput = resolve;
+            });
+
+            if (parentInputData) {
+                const inputData = parentInputData;
+                parentInputData = undefined;
+                if (inputData.done) break;
+                yield inputData.chunk;
+            }
+
+            const inputData = await inputPromise;
+            if (inputData.done) break;
+            yield inputData.chunk;
+        }
+    }
+
+    private pushChunk(chunk: OutputChunkType<TOpts>): void {
+        if (this.done) {
+            this.done(null, chunk);
+        } else {
+            this.push(chunk);
+        }
+    }
+
+    private pushError(error: Error): void {
+        if (this.done) {
+            this.done(error);
+        } else {
+            this.destroy(error);
+        }
+    }
+}
+
+export function transformFrom<TOpts extends Omit<stream.TransformOptions, 'transform' | 'flush'>>(
+    transformFn: TransformFunction<TOpts>,
     options?: TOpts,
 ): stream.Transform {
-    // TODO
+    return new TransformFromAsyncIterable(transformFn, options);
 }
