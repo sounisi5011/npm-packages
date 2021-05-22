3a4de83ad2cc104bc26d84ed79a30561a9fece58
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-05-22 22:56:25 +0900

    build(npm): include example code for RunKit in the package
    Maybe RunKit will not detect files that are not included in the package.

--- packages/stream-transform-from/.github/workflows/publish.sh
+++ packages/stream-transform-from/.github/workflows/publish.sh
@@ -3,6 +3,7 @@
 # https://qiita.com/yudoufu/items/48cb6fb71e5b498b2532#comment-87e291b98f4cabf77138
 readonly DIR_PATH="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
 
+rm "${DIR_PATH}/../../examples/package.json"
 outputs_tag_name="${outputs_tag_name}" \
   node "${DIR_PATH}/../../scripts/publish-convert-readme.js"
 
