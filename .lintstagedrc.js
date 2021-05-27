87e65cd69700a393238322038f43ad74dd478b17
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-05-27 09:42:11 +0900

    build(lint-staged): automatically update `README.md` when `package.json` is committed

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,11 +2,11 @@
 const path = require('path');
 
 /**
- * @param {string} basename
+ * @param {Array<string>} basenameList
  * @returns {function(string): boolean}
  */
-function baseFilter(basename) {
-  return filename => path.basename(filename) === basename;
+function baseFilter(...basenameList) {
+  return filename => basenameList.includes(path.basename(filename));
 }
 
 /**
@@ -16,6 +16,15 @@ function baseFilter(basename) {
 function extFilter(...extList) {
   extList = extList.map(ext => ext.replace(/^\.?/, '.'));
   return filename => extList.includes(path.extname(filename));
+}
+
+/**
+ * @template T
+ * @param {readonly T[]} array
+ * @returns {T[]}
+ */
+function unique(array) {
+  return [...new Set(array)];
 }
 
 module.exports = {
@@ -42,12 +51,16 @@ module.exports = {
       );
     }
 
-    const submoduleReadmeFiles = filenames
-      .filter(baseFilter('README.md'))
-      .filter(filename => path.dirname(path.resolve(filename)) !== __dirname);
+    const submoduleReadmeFiles = unique(
+      filenames
+        .filter(baseFilter('README.md', 'package.json'))
+        .filter(filename => path.dirname(path.resolve(filename)) !== __dirname)
+        .map(filename => path.join(path.dirname(filename), 'README.md')),
+    );
     if (submoduleReadmeFiles.length >= 1) {
       commands.push(
         `node ./scripts/update-readme-badge.js ${submoduleReadmeFiles.join(' ')}`,
+        `git add ${submoduleReadmeFiles.join(' ')}`,
       );
     }
 
