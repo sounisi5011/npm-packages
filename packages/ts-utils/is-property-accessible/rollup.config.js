bce5f4a8212e7372ea62a6bc10f3c5699ffd8cdf
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-05-30 15:31:37 +0900

    build(x): stop generating sourceMaps for CommonJS
    The `@betit/rollup-plugin-rename-extensions` ignores the TypeScript sourceMap and generates an incorrect sourceMap.
    Therefore, disable the generation of the sourceMap.

--- packages/ts-utils/is-property-accessible/rollup.config.js
+++ packages/ts-utils/is-property-accessible/rollup.config.js
@@ -3,17 +3,27 @@
 import renameExtensions from '@betit/rollup-plugin-rename-extensions';
 import typescript from '@rollup/plugin-typescript';
 
+// The "@betit/rollup-plugin-rename-extensions" ignores the TypeScript sourceMap and generates an incorrect sourceMap.
+// Therefore, disable the generation of the sourceMap.
+const generateSourceMap = false;
+
 /** @type {import('rollup').RollupOptions} */
 const options = {
   input: 'src/index.ts',
   output: {
     dir: 'dist',
     format: 'cjs',
-    sourcemap: true,
+    sourcemap: generateSourceMap,
   },
   plugins: [
     typescript({
       tsconfig: './src/tsconfig.json',
+      inlineSources: false,
+      sourceMap: generateSourceMap,
+      // Disables the generation of type definition files.
+      // Because the tsc command generates the type definition files.
+      declaration: false,
+      composite: false,
     }),
     renameExtensions({
       mappings: {
