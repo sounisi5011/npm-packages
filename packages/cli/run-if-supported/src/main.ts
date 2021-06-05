e3002d12fefed8880a47579de51bcef77ace85d2
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-06-05 16:20:12 +0900

    fix(run-if-supported): introduce `@sounisi5011/ts-utils-is-property-accessible`
    Replace `isRecordLike()` function with `@sounisi5011/ts-utils-is-property-accessible` package.

--- packages/cli/run-if-supported/src/main.ts
+++ packages/cli/run-if-supported/src/main.ts
@@ -1,18 +1,18 @@
-import { promises as fsAsync } from 'fs';
-import { dirname, resolve as resolvePath } from 'path';
-
+import { isPropAccessible } from '@sounisi5011/ts-utils-is-property-accessible';
 import { commandJoin } from 'command-join';
+import { promises as fsAsync } from 'fs';
 import { ArgumentError } from 'ow';
 import parseJson from 'parse-json';
+import { dirname, resolve as resolvePath } from 'path';
 import pkgUp from 'pkg-up';
 import type { JsonValue } from 'type-fest';
 
 import { isNotSupported } from './is-supported';
 import { parseOptions } from './options';
-import { filterObjectEntry, isRecordLike, isString } from './utils';
+import { filterObjectEntry, isString } from './utils';
 
 function getBinName(pkg: Record<PropertyKey, unknown>, entryFilepath: string): string | undefined {
-    if (isRecordLike(pkg['bin'])) {
+    if (isPropAccessible(pkg['bin'])) {
         const binEntry = Object.entries(pkg['bin'])
             .filter(filterObjectEntry(isString))
             .map(([binName, binPath]) => ({ binName, binPath }))
@@ -38,7 +38,7 @@ function getCliData(entryFilepath: string): {
     let version: string | undefined;
     let description = '';
 
-    if (!isRecordLike(PKG)) return { binName: undefined, version, description };
+    if (!isPropAccessible(PKG)) return { binName: undefined, version, description };
 
     const binName = getBinName(PKG, entryFilepath);
     if (typeof PKG['version'] === 'string') version = PKG['version'];
--- packages/cli/run-if-supported/src/utils.ts
+++ packages/cli/run-if-supported/src/utils.ts
@@ -1,7 +1,3 @@
-export function isRecordLike(value: unknown): value is Record<PropertyKey, unknown> {
-    return value !== null && value !== undefined;
-}
-
 export function isString(value: unknown): value is string {
     return typeof value === 'string';
 }
