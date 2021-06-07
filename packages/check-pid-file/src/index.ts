2de00a836e3682da3ff8cfadf5ef84087d68cde0
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-06-07 19:12:47 +0900

    fix(check-pid-file): fix bugs
    + Fix errors thrown by fs module to `Error` object with stack trace.
    + Logically reverse the return value of the `isProcessExist()` function

--- packages/check-pid-file/src/index.ts
+++ packages/check-pid-file/src/index.ts
@@ -5,13 +5,33 @@ import findProcess from 'find-process';
 import onExit from 'signal-exit';
 import writeFileAtomic from 'write-file-atomic';
 
+async function fixNodePrimordialsError<T>(promise: Promise<T>): Promise<T> {
+    return await promise.catch((oldError: Error) => {
+        if (oldError instanceof Error && typeof oldError.stack === 'string') {
+            throw oldError;
+        }
+
+        const newError = new Error(oldError.message);
+        const { message: _1, stack: _2, ...oldErrorProperties } = Object.getOwnPropertyDescriptors(oldError);
+        Object.defineProperties(newError, oldErrorProperties);
+        throw newError;
+    });
+}
+
+const readFileAsync = async (filepath: string): Promise<string> =>
+    await (
+        fixNodePrimordialsError(fsPromises.readFile(filepath, 'utf8'))
+    );
+
 async function createFile(filepath: string, data: string | Buffer | Uint8Array): Promise<boolean> {
-    return await fsPromises.writeFile(filepath, data, { flag: 'wx' })
-        .then(() => true)
-        .catch((error: Error & Record<string, unknown>) => {
-            if (error['code'] === 'EEXIST') return false;
-            throw error;
-        });
+    return await fixNodePrimordialsError(
+        fsPromises.writeFile(filepath, data, { flag: 'wx' })
+            .then(() => true)
+            .catch((error: Error & Record<string, unknown>) => {
+                if (error['code'] === 'EEXIST') return false;
+                throw error;
+            }),
+    );
 }
 
 async function isPidExist(pid: number): Promise<boolean> {
@@ -24,7 +44,7 @@ function parsePidFile(pidFileContent: string): number | null {
 }
 
 async function checkPidFileUpdateSuccessed(pidFilepath: string, pid: number): Promise<boolean> {
-    const pidFileContent = await fsPromises.readFile(pidFilepath, 'utf8');
+    const pidFileContent = await readFileAsync(pidFilepath);
     return parsePidFile(pidFileContent) === pid;
 }
 
@@ -37,7 +57,7 @@ export async function isProcessExist(pidFilepath: string, { pid = process.pid }:
     const pidFileContent = `${pid}\n`;
 
     if (!(await createFile(pidFilepath, pidFileContent))) {
-        const savedPid = parsePidFile(await fsPromises.readFile(pidFilepath, 'utf8'));
+        const savedPid = parsePidFile(await readFileAsync(pidFilepath));
         if (typeof savedPid !== 'number' || (savedPid !== pid && !(await isPidExist(savedPid)))) {
             await writeFileAtomic(pidFilepath, pidFileContent);
         }
@@ -49,5 +69,5 @@ export async function isProcessExist(pidFilepath: string, { pid = process.pid }:
             unlinkSync(pidFilepath);
         });
     }
-    return result;
+    return !result;
 }
