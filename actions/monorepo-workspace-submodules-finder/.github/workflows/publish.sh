48a56b1994213633fcae047f57af599c306359a7
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-06-28 20:42:57 +0900

    ci(publish): use `no-scope-package-name`

--- actions/monorepo-workspace-submodules-finder/.github/workflows/publish.sh
+++ actions/monorepo-workspace-submodules-finder/.github/workflows/publish.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+readonly MAJOR_TAG_NAME="${matrix_package_name_without_scope}-v${outputs_major}"
+readonly MINOR_TAG_NAME="${matrix_package_name_without_scope}-v${outputs_major}.${outputs_minor}"
+
 git config user.name  'github-actions[bot]'
 git config user.email 'github-actions[bot]@users.noreply.github.com'
 git remote add gh-token "https://${GITHUB_TOKEN}@github.com/sounisi5011/npm-packages.git"
-git tag -d "${matrix_package_name}-v${outputs_major}" || true
-git tag -d "${matrix_package_name}-v${outputs_major}.${outputs_minor}" || true
-git push origin :"${matrix_package_name}-v${outputs_major}" || true
-git push origin :"${matrix_package_name}-v${outputs_major}.${outputs_minor}" || true
-git tag -a "${matrix_package_name}-v${outputs_major}" -m "Release ${matrix_package_name}@${outputs_major}.x"
-git tag -a "${matrix_package_name}-v${outputs_major}.${outputs_minor}" -m "Release ${matrix_package_name}@${outputs_major}.${outputs_minor}.x"
-git push origin "${matrix_package_name}-v${outputs_major}"
-git push origin "${matrix_package_name}-v${outputs_major}.${outputs_minor}"
+git tag -d "${MAJOR_TAG_NAME}" || true
+git tag -d "${MINOR_TAG_NAME}" || true
+git push origin :"${MAJOR_TAG_NAME}" || true
+git push origin :"${MINOR_TAG_NAME}" || true
+git tag -a "${MAJOR_TAG_NAME}" -m "Release ${matrix_package_name_with_scope}@${outputs_major}.x"
+git tag -a "${MINOR_TAG_NAME}" -m "Release ${matrix_package_name_with_scope}@${outputs_major}.${outputs_minor}.x"
+git push origin "${MAJOR_TAG_NAME}"
+git push origin "${MINOR_TAG_NAME}"
