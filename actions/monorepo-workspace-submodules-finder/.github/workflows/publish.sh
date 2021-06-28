89ed01d38cc5b534af5dc35a0b8354e9f771658d
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2021-06-28 21:13:21 +0900

    ci(monorepo-workspace-submodules-finder-action): add the update_git_tag function in `.github/workflows/publish.sh` script

--- actions/monorepo-workspace-submodules-finder/.github/workflows/publish.sh
+++ actions/monorepo-workspace-submodules-finder/.github/workflows/publish.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-readonly MAJOR_TAG_NAME="${matrix_package_name_without_scope}-v${outputs_major}"
-readonly MINOR_TAG_NAME="${matrix_package_name_without_scope}-v${outputs_major}.${outputs_minor}"
-
 git config user.name  'github-actions[bot]'
 git config user.email 'github-actions[bot]@users.noreply.github.com'
 git remote add gh-token "https://${GITHUB_TOKEN}@github.com/sounisi5011/npm-packages.git"
-git tag -d "${MAJOR_TAG_NAME}" || true
-git tag -d "${MINOR_TAG_NAME}" || true
-git push origin :"${MAJOR_TAG_NAME}" || true
-git push origin :"${MINOR_TAG_NAME}" || true
-git tag -a "${MAJOR_TAG_NAME}" -m "Release ${matrix_package_name_with_scope}@${outputs_major}.x"
-git tag -a "${MINOR_TAG_NAME}" -m "Release ${matrix_package_name_with_scope}@${outputs_major}.${outputs_minor}.x"
-git push origin "${MAJOR_TAG_NAME}"
-git push origin "${MINOR_TAG_NAME}"
+
+update_git_tag() {
+  local tag_name="$1"
+  local tag_message="$2"
+  git tag -d "${tag_name}" || true
+  git push origin :"${tag_name}" || true
+  git tag -a "${tag_name}" -m "${tag_message}"
+  git push origin "${tag_name}"
+}
+
+update_git_tag "${matrix_package_name_without_scope}-v${outputs_major}"                  "Release ${matrix_package_name_with_scope}@${outputs_major}.x"
+update_git_tag "${matrix_package_name_without_scope}-v${outputs_major}.${outputs_minor}" "Release ${matrix_package_name_with_scope}@${outputs_major}.${outputs_minor}.x"
