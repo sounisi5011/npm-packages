0fe3ab0c82a2f36ca4be8cccf20a52aa810b0788
sounisi5011 <sounisi5011@users.noreply.github.com>
2021-06-28 13:20:55 +0900

    ci(monorepo-workspace-submodules-finder-action): auto update package list

---
+++ actions/monorepo-workspace-submodules-finder/.github/workflows/post-release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+exec_group() {
+  local quoted_args=() exit_code
+
+  # see https://qiita.com/kawaz/items/f8d68f11d31aa3ea3d1c
+  for arg in "$@"; do
+    # see https://stackoverflow.com/a/229606/4907315
+    if [[ "${arg}" == *' '* ]]; then
+      quoted_args+=("'${arg}'")
+    else
+      quoted_args+=("${arg}")
+    fi
+  done
+
+  # see https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#grouping-log-lines
+  echo '::group::$' "${quoted_args[*]}"
+  "$@"
+  exit_code=$?
+  echo '::endgroup::'
+
+  return $exit_code
+}
+
+exec_group pnpm run -w build:package-list || true
+exec_group git add ../../README.md || true
+exec_group git commit -m 'docs: update package list' || true
