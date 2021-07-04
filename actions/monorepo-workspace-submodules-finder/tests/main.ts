4ade05d361f458909e2bb4b0f328c1b6a6c63b92
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2021-07-04 14:28:35 +0900

    test(monorepo-workspace-submodules-finder-action): add tests for `excludeUnchangedSubmodules()` function

--- actions/monorepo-workspace-submodules-finder/tests/main.ts
+++ actions/monorepo-workspace-submodules-finder/tests/main.ts
@@ -1,7 +1,9 @@
 import type { hasOwnProperty } from '@sounisi5011/ts-type-util-has-own-property';
+import nock from 'nock';
+import spawk from 'spawk';
 
 import pkg from '../package.json';
-import { getPackageDataList, PackageData } from '../src/main';
+import { excludeUnchangedSubmodules, getPackageDataList, PackageData } from '../src/main';
 
 describe('getPackageDataList()', () => {
     it('package data should be included', async () => {
@@ -16,5 +18,119 @@ describe('getPackageDataList()', () => {
                 : false,
         };
         await expect(getPackageDataList()).resolves.toContainEqual(packageData);
+    });
+});
+
+describe('excludeUnchangedSubmodules()', () => {
+    const gitRegExp = /^\/?(?:[^/]+\/)*git$/;
+    const submoduleList = [
+        { 'path-git-relative': 'mod1' },
+        { 'path-git-relative': 'dir1/mod2' },
+        { 'path-git-relative': 'mod3' },
+        { 'path-git-relative': 'dir2/mod4' },
+    ];
+
+    beforeEach(() => {
+        spawk.clean();
+        spawk.preventUnmatched();
+
+        nock.disableNetConnect();
+    });
+
+    it('since: initial commit', async () => {
+        const result = excludeUnchangedSubmodules(submoduleList, {
+            api: {
+                owner: 'foo',
+                repo: 'bar',
+                token: 'hoge',
+            },
+            info: () => null,
+            debug: () => null,
+            group: async (_, fn) => await fn(),
+            since: 'initial commit',
+        });
+        await expect(result).resolves.toStrictEqual(submoduleList);
+    });
+
+    describe('since: latest release', () => {
+        const since = 'latest release' as const;
+
+        it('not released yet', async () => {
+            nock('https://api.github.com')
+                .get('/repos/foo/bar/releases/latest')
+                .reply(404, {
+                    'message': 'Not Found',
+                    'documentation_url': 'https://docs.github.com/rest/reference/repos#get-the-latest-release',
+                });
+
+            const result = excludeUnchangedSubmodules(submoduleList, {
+                api: {
+                    owner: 'foo',
+                    repo: 'bar',
+                    token: 'hoge',
+                },
+                info: () => null,
+                debug: () => null,
+                group: async (_, fn) => await fn(),
+                since,
+            });
+            await expect(result).resolves.toStrictEqual(submoduleList);
+        });
+
+        it('no diff', async () => {
+            nock('https://api.github.com')
+                .get('/repos/foo/bar/releases/latest')
+                .reply(200, {
+                    tag_name: 'fuga',
+                });
+            spawk.spawn(gitRegExp, ['fetch', '--no-tags', 'origin', 'tag', 'fuga']);
+            spawk.spawn(gitRegExp, ['diff', '--name-only', 'fuga'])
+                .stdout('');
+
+            const result = excludeUnchangedSubmodules(submoduleList, {
+                api: {
+                    owner: 'foo',
+                    repo: 'bar',
+                    token: 'hoge',
+                },
+                info: () => null,
+                debug: () => null,
+                group: async (_, fn) => await fn(),
+                since,
+            });
+            await expect(result).resolves.toStrictEqual([]);
+        });
+
+        it('has diff: same module', async () => {
+            nock('https://api.github.com')
+                .get('/repos/foo/bar/releases/latest')
+                .reply(200, {
+                    tag_name: 'fuga',
+                });
+            spawk.spawn(gitRegExp, ['fetch', '--no-tags', 'origin', 'tag', 'fuga']);
+            spawk.spawn(gitRegExp, ['diff', '--name-only', 'fuga'])
+                .stdout([
+                    'mod1/file',
+                    'dir2/mod4',
+                    'mod3',
+                ].join('\n'));
+
+            const result = excludeUnchangedSubmodules(submoduleList, {
+                api: {
+                    owner: 'foo',
+                    repo: 'bar',
+                    token: 'hoge',
+                },
+                info: () => null,
+                debug: () => null,
+                group: async (_, fn) => await fn(),
+                since,
+            });
+            await expect(result).resolves.toStrictEqual([
+                { 'path-git-relative': 'mod1' },
+                { 'path-git-relative': 'mod3' },
+                { 'path-git-relative': 'dir2/mod4' },
+            ]);
+        });
     });
 });
