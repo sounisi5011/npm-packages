d50a46eaf89b6e1dae23e667dc687d91d880ffdb
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2021-07-30 13:06:35 +0900

    refactor(jest-binary-data-matchers): reduce similar blocks
    Code Climate reported:
    + Similar blocks of code found in 3 locations. Consider refactoring.

--- packages/jest-matchers/binary-data/src/matchers.ts
+++ packages/jest-matchers/binary-data/src/matchers.ts
@@ -3,8 +3,7 @@ import { EXPECTED_COLOR, matcherHint, RECEIVED_COLOR } from 'jest-matcher-utils'
 import { byteSize, toMessageFn } from './utils';
 import { ensureByteSize } from './utils/jest';
 
-function compareByteSize(
-    context: jest.MatcherContext,
+function createCompareByteSizeMatcher(
     opts: {
         matcherName: string;
         operator?: string;
@@ -12,29 +11,36 @@ function compareByteSize(
             expected: number | bigint;
             received: number | bigint;
         }) => boolean;
-        received: unknown;
-        expected: number | bigint;
     },
-): jest.CustomMatcherResult {
-    const isNot = context.isNot;
-    const options: jest.MatcherHintOptions = {
-        isNot,
-        promise: context.promise,
-    };
+) {
+    /**
+     * @see https://github.com/facebook/jest/blob/v27.0.6/packages/expect/src/matchers.ts#L234-L256
+     */
+    return function(
+        this: jest.MatcherContext,
+        received: unknown,
+        expected: number | bigint,
+    ): jest.CustomMatcherResult {
+        const isNot = this.isNot;
+        const options: jest.MatcherHintOptions = {
+            isNot,
+            promise: this.promise,
+        };
 
-    const { matcherName, received, expected } = opts;
-    ensureByteSize(received, expected, matcherName, options);
+        const { matcherName } = opts;
+        ensureByteSize(received, expected, matcherName, options);
 
-    const operator: string = opts.operator ? ` ${opts.operator}` : '';
-    const opIndent = ' '.repeat(operator.length);
-    return {
-        message: toMessageFn(() => [
-            matcherHint(matcherName, undefined, undefined, options),
-            ``,
-            `Expected:${isNot ? ' not' : ''}${operator} ${EXPECTED_COLOR(byteSize(expected))}`,
-            `Received:${isNot ? '    ' : ''}${opIndent} ${RECEIVED_COLOR(byteSize(received))}`,
-        ]),
-        pass: opts.passFn({ expected, received }),
+        const operator: string = opts.operator ? ` ${opts.operator}` : '';
+        const opIndent = ' '.repeat(operator.length);
+        return {
+            message: toMessageFn(() => [
+                matcherHint(matcherName, undefined, undefined, options),
+                ``,
+                `Expected:${isNot ? ' not' : ''}${operator} ${EXPECTED_COLOR(byteSize(expected))}`,
+                `Received:${isNot ? '    ' : ''}${opIndent} ${RECEIVED_COLOR(byteSize(received))}`,
+            ]),
+            pass: opts.passFn({ expected, received }),
+        };
     };
 }
 
@@ -42,83 +48,43 @@ function compareByteSize(
  * @see https://github.com/facebook/jest/blob/v27.0.6/packages/expect/src/matchers.ts#L234-L256
  * @see https://github.com/facebook/jest/blob/v27.0.6/packages/expect/src/matchers.ts#L74-L125
  */
-export function toBeByteSize(
-    this: jest.MatcherContext,
-    received: number | bigint,
-    expected: number | bigint,
-): jest.CustomMatcherResult {
-    return compareByteSize(this, {
-        matcherName: toBeByteSize.name,
-        passFn: ({ expected, received }) => received == expected, // eslint-disable-line eqeqeq
-        received,
-        expected,
-    });
-}
+export const toBeByteSize = createCompareByteSizeMatcher({
+    matcherName: 'toBeByteSize',
+    passFn: ({ expected, received }) => received == expected, // eslint-disable-line eqeqeq
+});
 
 /**
  * @see https://github.com/facebook/jest/blob/v27.0.6/packages/expect/src/matchers.ts#L234-L256
  */
-export function toBeGreaterThanByteSize(
-    this: jest.MatcherContext,
-    received: number | bigint,
-    expected: number | bigint,
-): jest.CustomMatcherResult {
-    return compareByteSize(this, {
-        matcherName: toBeGreaterThanByteSize.name,
-        operator: '>',
-        passFn: ({ expected, received }) => received > expected,
-        received,
-        expected,
-    });
-}
+export const toBeGreaterThanByteSize = createCompareByteSizeMatcher({
+    matcherName: 'toBeGreaterThanByteSize',
+    operator: '>',
+    passFn: ({ expected, received }) => received > expected,
+});
 
 /**
  * @see https://github.com/facebook/jest/blob/v27.0.6/packages/expect/src/matchers.ts#L258-L280
  */
-export function toBeGreaterThanOrEqualByteSize(
-    this: jest.MatcherContext,
-    received: number | bigint,
-    expected: number | bigint,
-): jest.CustomMatcherResult {
-    return compareByteSize(this, {
-        matcherName: toBeGreaterThanOrEqualByteSize.name,
-        operator: '>=',
-        passFn: ({ expected, received }) => received >= expected,
-        received,
-        expected,
-    });
-}
+export const toBeGreaterThanOrEqualByteSize = createCompareByteSizeMatcher({
+    matcherName: 'toBeGreaterThanOrEqualByteSize',
+    operator: '>=',
+    passFn: ({ expected, received }) => received >= expected,
+});
 
 /**
  * @see https://github.com/facebook/jest/blob/v27.0.6/packages/expect/src/matchers.ts#L332-L354
  */
-export function toBeLessThanByteSize(
-    this: jest.MatcherContext,
-    received: number | bigint,
-    expected: number | bigint,
-): jest.CustomMatcherResult {
-    return compareByteSize(this, {
-        matcherName: toBeLessThanByteSize.name,
-        operator: '<',
-        passFn: ({ expected, received }) => received < expected,
-        received,
-        expected,
-    });
-}
+export const toBeLessThanByteSize = createCompareByteSizeMatcher({
+    matcherName: 'toBeLessThanByteSize',
+    operator: '<',
+    passFn: ({ expected, received }) => received < expected,
+});
 
 /**
  * @see https://github.com/facebook/jest/blob/v27.0.6/packages/expect/src/matchers.ts#L356-L378
  */
-export function toBeLessThanOrEqualByteSize(
-    this: jest.MatcherContext,
-    received: number | bigint,
-    expected: number | bigint,
-): jest.CustomMatcherResult {
-    return compareByteSize(this, {
-        matcherName: toBeLessThanOrEqualByteSize.name,
-        operator: '<=',
-        passFn: ({ expected, received }) => received <= expected,
-        received,
-        expected,
-    });
-}
+export const toBeLessThanOrEqualByteSize = createCompareByteSizeMatcher({
+    matcherName: 'toBeLessThanOrEqualByteSize',
+    operator: '<=',
+    passFn: ({ expected, received }) => received <= expected,
+});
