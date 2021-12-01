79db2b858ba60825d59179882f0ef630c6a35936
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2021-12-01 19:48:03 +0900

    revert(encrypted-archive): test(encrypted-archive): excessive use of memory
    This reverts commit 9d78815cf18c4c9f850a3c813cbbe19b9d43eeed.

--- packages/encrypted-archive/tests/stream/encrypt-and-decrypt-then-generate-sha1.cjs
+++ packages/encrypted-archive/tests/stream/encrypt-and-decrypt-then-generate-sha1.cjs
@@ -23,14 +23,6 @@ let timerID;
   timerID = setImmediate(monitorMem);
 })();
 
-const buffer = new Uint8Array(500 * 2 ** 20);
-let bufferOffset = 0;
-
-process.stdin.on('data', data => {
-  data.copy(buffer, bufferOffset);
-  bufferOffset += data.byteLength;
-});
-
 pipeline(
   process.stdin,
   encryptStream(password, {
