681a75ce28ed5a4eea8008f9b06a657727cae181
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2021-12-01 19:10:54 +0900

    test(encrypted-archive): monitor memory usage of sub-processes instead of test code
    When monitoring memory usage in the test code, the difference in memory usage may become negative due to garbage collection.
    Also, Jest does not necessarily run each test in a different sub-process.
    
    Therefore, we changed to run encryption and decryption in a sub-process and use the memory usage of the sub-process for testing.

---
+++ packages/encrypted-archive/tests/stream/encrypt-and-decrypt-then-generate-sha1.cjs
@@ -0,0 +1,42 @@
+// @ts-check
+
+const { createHash } = require('crypto');
+const { pipeline } = require('stream');
+
+const { decryptStream, encryptStream } = require('../../dist');
+
+const password = '1234';
+const outputHash = createHash('sha1');
+
+const maxMemoryUsage = process.memoryUsage();
+
+/** @type {NodeJS.Immediate} */
+let timerID;
+(function monitorMem() {
+  const memoryUsage = process.memoryUsage();
+  maxMemoryUsage.rss = Math.max(maxMemoryUsage.rss, memoryUsage.rss);
+  maxMemoryUsage.heapTotal = Math.max(maxMemoryUsage.heapTotal, memoryUsage.heapTotal);
+  maxMemoryUsage.heapUsed = Math.max(maxMemoryUsage.heapUsed, memoryUsage.heapUsed);
+  maxMemoryUsage.external = Math.max(maxMemoryUsage.external, memoryUsage.external);
+  maxMemoryUsage.arrayBuffers = Math.max(maxMemoryUsage.arrayBuffers, memoryUsage.arrayBuffers);
+
+  timerID = setImmediate(monitorMem);
+})();
+
+pipeline(
+  process.stdin,
+  encryptStream(password, {
+    algorithm: 'aes-256-gcm',
+  }),
+  decryptStream(password),
+  outputHash,
+  err => {
+    clearImmediate(timerID);
+
+    if (err) throw err;
+    console.log(JSON.stringify({
+      maxMemoryUsage,
+      sha1: outputHash.digest('hex'),
+    }));
+  },
+);
--- packages/encrypted-archive/tests/stream/large-data.ts
+++ packages/encrypted-archive/tests/stream/large-data.ts
@@ -1,43 +1,48 @@
 import * as crypto from 'crypto';
+import * as path from 'path';
 
-import { decryptStream, encryptStream } from '../../src';
+import execa from 'execa';
+
 import { createFillBytesReadableStream, pipelineAsync } from '../helpers/stream';
 
-const password = '1234';
+const PACKAGE_ROOT = path.resolve(__dirname, '../..');
 
 describe('encryptStream()', () => {
+    beforeAll(async () => {
+        await execa('pnpm', ['run', 'build-with-cache'], { cwd: PACKAGE_ROOT });
+    }, 60 * 1000);
+
     it('transform large data', async () => {
         const dataSize = 500 * 2 ** 20; // 500 MiB
         const inputHash = crypto.createHash('sha1');
-        const outputHash = crypto.createHash('sha1');
         const inputStream = createFillBytesReadableStream({ size: dataSize });
 
-        const startMem = process.memoryUsage().rss;
+        const subprocessPath = path.resolve(__dirname, 'encrypt-and-decrypt-then-generate-sha1.cjs');
+        const subprocess = execa('node', [subprocessPath], { input: inputStream });
+
         await Promise.all([
-            pipelineAsync(
-                inputStream,
-                encryptStream(password, {
-                    algorithm: 'aes-256-gcm',
-                }),
-                decryptStream(password),
-                outputHash,
-            ),
+            subprocess,
             pipelineAsync(
                 inputStream,
                 inputHash,
             ),
         ]);
-        const endMem = process.memoryUsage().rss;
+        const subprocessResult = JSON.parse((await subprocess).stdout);
 
         /**
          * If Encryptor is using the Stream API correctly,
          * the amount of memory used will be less than the input data length
          * even when converting huge data.
          */
-        expect(endMem - startMem).toBeLessThanByteSize(dataSize);
+        const expectedMaxMemoryUsage = dataSize / 5;
+        expect(subprocessResult.maxMemoryUsage.rss).toBeLessThanByteSize(expectedMaxMemoryUsage);
+        expect(subprocessResult.maxMemoryUsage.heapTotal).toBeLessThanByteSize(expectedMaxMemoryUsage);
+        expect(subprocessResult.maxMemoryUsage.heapUsed).toBeLessThanByteSize(expectedMaxMemoryUsage);
+        expect(subprocessResult.maxMemoryUsage.external).toBeLessThanByteSize(expectedMaxMemoryUsage);
+        expect(subprocessResult.maxMemoryUsage.arrayBuffers).toBeLessThanByteSize(expectedMaxMemoryUsage);
 
         const inputSHA1 = inputHash.digest('hex');
-        const outputSha1 = outputHash.digest('hex');
+        const outputSha1 = subprocessResult.sha1;
         expect(outputSha1).toStrictEqual(inputSHA1);
     }, 10 * 60 * 1000);
 });
