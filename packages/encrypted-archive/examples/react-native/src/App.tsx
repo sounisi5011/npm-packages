5be2513570aa13c219a1fb4cfefb90e0b960284a
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2021-12-20 20:23:25 +0900

    fix(encrypted-archive): fix errors in example code for React Native
    + Stop using path alias in `tsconfig.json`
    
        ESLint does not detect path alias.
        Also, the pre-built source code should not be available.
    
    + Add return type in `examples/react-native/src/App.tsx`
    
    + Add correct error handling in `examples/react-native/src/App.tsx`

--- packages/encrypted-archive/examples/react-native/src/App.tsx
+++ packages/encrypted-archive/examples/react-native/src/App.tsx
@@ -1,17 +1,24 @@
-import { multiply } from '@sounisi5011/encrypted-archive';
+import { multiply } from '@sounisi5011/encrypted-archive/dist/index.react-native';
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App(): JSX.Element {
     const [result, setResult] = React.useState<number | undefined>();
+    const [error, setError] = React.useState<unknown>();
 
     React.useEffect(() => {
-        multiply(3, 7).then(setResult);
+        multiply(3, 7)
+            .then(setResult)
+            .catch(setError);
     }, []);
 
     return (
         <View style={styles.container}>
-            <Text>Result: {result}</Text>
+            {error
+                ? <Text>{String(error)}</Text>
+                : result !== undefined
+                ? <Text>Result: {result}</Text>
+                : <Text>Calculating...</Text>}
         </View>
     );
 }
