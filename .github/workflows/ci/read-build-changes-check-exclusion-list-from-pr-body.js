2cc34db3dff59afdaee2a762c1ac526273b4cf36
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2022-01-12 20:15:30 +0900

    ci(build result check): add comments

--- .github/workflows/ci/read-build-changes-check-exclusion-list-from-pr-body.js
+++ .github/workflows/ci/read-build-changes-check-exclusion-list-from-pr-body.js
@@ -1,3 +1,14 @@
+/**
+ * Parses the pull request body specified in the PR_BODY environment variable and returns a list of files that should be excluded.
+ * This script will parse Markdown like this:
+ *
+ *     ###### ci: build changes check exclusion list
+ *
+ *     ```
+ *     foo/bar.txt
+ *     path/to/
+ *     ```
+ */
 const pattern = /(?<=^#+ *ci: +build +changes +check +(?:exclude|exclusion) +list\s+^(`{3,})\s+)^.+(?=^\1`*\s*$)/ims;
 const match = pattern.exec(process.env.PR_BODY);
 if (match) {
