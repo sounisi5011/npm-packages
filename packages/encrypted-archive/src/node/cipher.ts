f5b85be15dacb3cc86204e5d4ecb2d83cb49ece2
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2022-01-16 18:53:04 +0900

    refactor(encrypted-archive): reduce lines in `getCryptoAlgorithm()` function
    Code Climate reported:
    + Function `getCryptoAlgorithm` has 29 lines of code (exceeds 25 allowed). Consider refactoring.

--- packages/encrypted-archive/src/node/cipher.ts
+++ packages/encrypted-archive/src/node/cipher.ts
@@ -1,27 +1,53 @@
 import { createCipheriv, createDecipheriv } from 'crypto';
 import type * as crypto from 'crypto';
 
-import type { CryptoAlgorithmName, GetCryptoAlgorithm } from '../core/types/crypto';
+import type { CryptoAlgorithmData, CryptoAlgorithmName, GetCryptoAlgorithm } from '../core/types/crypto';
 import { uint8arrayConcat } from '../core/utils';
 import { fixNodePrimordialsErrorInstance } from './utils';
 
-const cryptoAlgorithmList = [
+const cryptoAlgorithmList: readonly CryptoAlgorithmData[] = [
     (() => {
         const ALGORITHM_NAME = 'aes-256-gcm';
 
         return {
-            name: ALGORITHM_NAME,
+            algorithmName: ALGORITHM_NAME,
             keyLength: 256 / 8,
             /**
              * @see https://scrapbox.io/nwtgck/AES-GCM%E3%81%AE%E5%88%9D%E6%9C%9F%E5%8C%96%E3%83%99%E3%82%AF%E3%83%88%E3%83%ABNONCE%E3%81%AF12%E3%83%90%E3%82%A4%E3%83%88(96%E3%83%93%E3%83%83%E3%83%88)%E3%81%8C%E6%8E%A8%E5%A5%A8
              * @see https://crypto.stackexchange.com/a/26787
              */
             nonceLength: 96 / 8,
-            createCipher: (key: crypto.CipherKey, nonce: crypto.BinaryLike) =>
-                createCipheriv(ALGORITHM_NAME, key, nonce),
-            createDecipher: (key: crypto.BinaryLike, nonce: crypto.BinaryLike) =>
-                createDecipheriv(ALGORITHM_NAME, key, nonce),
-        } as const;
+            async encrypt({ key, nonce, cleartext }) {
+                /**
+                 * Encrypt cleartext
+                 */
+                const cipher = createCipheriv(ALGORITHM_NAME, key, nonce);
+                const ciphertext = uint8arrayConcat(
+                    cipher.update(cleartext),
+                    cipher.final(),
+                );
+
+                /**
+                 * Get authentication tag
+                 */
+                const authTag = cipher.getAuthTag();
+
+                return { authTag, ciphertext };
+            },
+            async decrypt({ key, nonce, authTag, ciphertext }) {
+                try {
+                    const decipher = createDecipheriv(ALGORITHM_NAME, key, nonce);
+                    decipher.setAuthTag(authTag);
+                    const cleartext = uint8arrayConcat(
+                        decipher.update(ciphertext),
+                        decipher.final(),
+                    );
+                    return { cleartext };
+                } catch (error) {
+                    fixNodePrimordialsErrorInstance(error);
+                }
+            },
+        };
     })(),
     (() => {
         const ALGORITHM_NAME = 'chacha20-poly1305';
@@ -37,7 +63,7 @@ const cryptoAlgorithmList = [
         const algorithm: crypto.CipherCCMTypes = ALGORITHM_NAME;
 
         return {
-            name: ALGORITHM_NAME,
+            algorithmName: ALGORITHM_NAME,
             /**
              * @see https://tools.ietf.org/html/rfc8103#section-1.1
              * @see https://tools.ietf.org/html/rfc7539#section-2.8
@@ -48,54 +74,41 @@ const cryptoAlgorithmList = [
              * @see https://tools.ietf.org/html/rfc7539#section-2.8
              */
             nonceLength: 96 / 8,
-            createCipher: (key: crypto.CipherKey, nonce: crypto.BinaryLike) =>
-                createCipheriv(algorithm, key, nonce, { authTagLength: AUTH_TAG_LEN }),
-            createDecipher: (key: crypto.BinaryLike, nonce: crypto.BinaryLike) =>
-                createDecipheriv(algorithm, key, nonce, { authTagLength: AUTH_TAG_LEN }),
-        } as const;
+            async encrypt({ key, nonce, cleartext }) {
+                /**
+                 * Encrypt cleartext
+                 */
+                const cipher = createCipheriv(algorithm, key, nonce, { authTagLength: AUTH_TAG_LEN });
+                const ciphertext = uint8arrayConcat(
+                    cipher.update(cleartext),
+                    cipher.final(),
+                );
+
+                /**
+                 * Get authentication tag
+                 */
+                const authTag = cipher.getAuthTag();
+
+                return { authTag, ciphertext };
+            },
+            async decrypt({ key, nonce, authTag, ciphertext }) {
+                try {
+                    const decipher = createDecipheriv(algorithm, key, nonce, { authTagLength: AUTH_TAG_LEN });
+                    decipher.setAuthTag(authTag);
+                    const cleartext = uint8arrayConcat(
+                        decipher.update(ciphertext),
+                        decipher.final(),
+                    );
+                    return { cleartext };
+                } catch (error) {
+                    fixNodePrimordialsErrorInstance(error);
+                }
+            },
+        };
     })(),
 ];
 
-const cryptoAlgorithmMap = new Map(cryptoAlgorithmList.map(data => [data.name, data]));
+const cryptoAlgorithmMap = new Map(cryptoAlgorithmList.map(data => [data.algorithmName, data]));
 export const defaultCryptoAlgorithmName: CryptoAlgorithmName = 'chacha20-poly1305';
 
-export const getCryptoAlgorithm: GetCryptoAlgorithm = algorithmName => {
-    const algorithm = cryptoAlgorithmMap.get(algorithmName);
-    if (!algorithm) return undefined;
-
-    return {
-        algorithmName,
-        keyLength: algorithm.keyLength,
-        nonceLength: algorithm.nonceLength,
-        async encrypt({ key, nonce, cleartext }) {
-            /**
-             * Encrypt cleartext
-             */
-            const cipher = algorithm.createCipher(key, nonce);
-            const ciphertext = uint8arrayConcat(
-                cipher.update(cleartext),
-                cipher.final(),
-            );
-
-            /**
-             * Get authentication tag
-             */
-            const authTag = cipher.getAuthTag();
-
-            return { authTag, ciphertext };
-        },
-        async decrypt({ key, nonce, authTag, ciphertext }) {
-            try {
-                const decipher = algorithm.createDecipher(key, nonce);
-                decipher.setAuthTag(authTag);
-                const cleartext = uint8arrayConcat(
-                    decipher.update(ciphertext),
-                    decipher.final(),
-                );
-                return { cleartext };
-            } catch (error) {
-                fixNodePrimordialsErrorInstance(error);
-            }
-        },
-    };
-};
+export const getCryptoAlgorithm: GetCryptoAlgorithm = algorithmName => cryptoAlgorithmMap.get(algorithmName);
