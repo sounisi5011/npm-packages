473988ba08a22e628a22bf68a9b1350f0719d282
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2022-05-25 22:25:58 +0900

    test(jest): find the `.ts` file in the path with the extension `.js`
    In ES Modules, the file extension cannot be omitted.
    TypeScript can read `.ts` files with the `.js` extension.
    So add the `moduleNameMapper` option and let Jest find `.ts` files.

--- jest.config.base.cjs
+++ jest.config.base.cjs
@@ -4,4 +4,13 @@ module.exports = {
    * So change resolver to `jest-node-exports-resolver`.
    */
   resolver: 'jest-node-exports-resolver',
+  /**
+   * In ES Modules, the file extension cannot be omitted.
+   * TypeScript can read `.ts` files with the `.js` extension.
+   * So add the `moduleNameMapper` option and let Jest find `.ts` files.
+   * @see https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+   */
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
