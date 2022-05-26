26f6bafd6da3a92fe00095ee3acc4d8c2db0cc0b
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2022-05-26 22:13:18 +0900

    build(lint-staged): fix configuration to be not dependent on cwd

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -7,6 +7,14 @@ const { promisify } = require('util');
 const execFileAsync = promisify(execFile);
 
 /**
+ * @param {string} pathname
+ * @returns {string}
+ */
+function rootPath(pathname) {
+  return path.resolve(__dirname, pathname);
+}
+
+/**
  * @param {string|RegExp} basename
  * @returns {function(string): boolean}
  */
@@ -26,6 +34,23 @@ function extFilter(...extList) {
 }
 
 /**
+ * @param {[string, ...string[]]} dirnameSegments
+ * @returns {function(string): boolean}
+ */
+function dirnameFilter(...dirnameSegments) {
+  const dirname = path.resolve(...dirnameSegments);
+  return filename => path.dirname(path.resolve(filename)) === dirname;
+}
+
+/**
+ * @param {function(string): boolean} fn
+ * @returns {function(string): boolean}
+ */
+function not(fn) {
+  return filename => !fn(filename);
+}
+
+/**
  * @template T
  * @param {readonly T[]} array
  * @returns {T[]}
@@ -41,7 +66,7 @@ function unique(array) {
  */
 async function dprintCommandList(filenames, config) {
   if (filenames.length < 1) return [];
-  const configOption = ['-c', path.resolve(__dirname, config)];
+  const configOption = ['-c', rootPath(config)];
 
   const { stdout } = await execFileAsync('pnpm', ['exec', 'dprint', 'output-file-paths', ...configOption]);
   const dprintTargetFilepathList = stdout
@@ -70,7 +95,10 @@ module.exports = async filenames => {
   /** @type {string[]} */
   const commands = [];
 
-  commands.push(`prettier --write ${filenames.join(' ')}`);
+  // Prettier will find the configuration files based on cwd.
+  // If inside submodule directories, Prettier will fail because cwd is not the project root.
+  // To work around this, Prettier is run using the project root npm-script.
+  commands.push(`pnpm run --workspace-root root:prettier -- --write ${filenames.join(' ')}`);
 
   const pkgFiles = filenames.filter(baseFilter('package.json'));
   if (pkgFiles.length >= 1) {
@@ -80,13 +108,14 @@ module.exports = async filenames => {
     );
   }
 
+  const rootFilenames = filenames.filter(dirnameFilter(__dirname));
   if (
-    filenames.some(baseFilter('.eslintignore'))
-    || filenames.some(baseFilter(/^(?:dprint|\.dprint)(?:-.*)?\.json$/))
+    rootFilenames.some(baseFilter('.eslintignore'))
+    || rootFilenames.some(baseFilter(/^(?:dprint|\.dprint)(?:-.*)?\.json$/))
   ) {
     commands.push(
-      'pnpm run build:dprint-config',
-      'git add ./.dprint.json ./.dprint-*.json',
+      'pnpm run --workspace-root build:dprint-config',
+      `git add ${['./.dprint.json', './.dprint-*.json'].map(rootPath).join(' ')}`,
     );
   }
 
@@ -114,28 +143,33 @@ module.exports = async filenames => {
   const tsOrJsFiles = [...tsFiles, ...jsFiles];
   if (tsOrJsFiles.length >= 1) {
     commands.push(
-      `eslint --cache --fix ${tsOrJsFiles.join(' ')}`,
+      // ESLint will find the configuration files based on cwd.
+      // If inside submodule directories, ESLint will fail because cwd is not the project root.
+      // To work around this, ESLint is run using the project root npm-script.
+      `pnpm run --workspace-root root:eslint -- --cache --fix ${tsOrJsFiles.join(' ')}`,
     );
   }
 
-  const readmeFiles = filenames.filter(baseFilter('README.md'));
-  const packageListFiles = filenames.filter(baseFilter('.package-list.js'));
-  if (pkgFiles.length >= 1 || readmeFiles.length >= 1 || packageListFiles.length >= 1) {
+  if (
+    pkgFiles.length >= 1
+    || rootFilenames.some(baseFilter('README.md'))
+    || rootFilenames.some(baseFilter('.package-list.js'))
+  ) {
     commands.push(
-      'run-s build:package-list',
-      'git add ./README.md',
+      'pnpm run --workspace-root build:package-list',
+      `git add ${rootPath('README.md')}`,
     );
   }
 
   const submoduleReadmeFiles = unique(
-    [...readmeFiles, ...pkgFiles]
-      .filter(filename => path.dirname(path.resolve(filename)) !== __dirname)
+    [...filenames.filter(baseFilter('README.md')), ...pkgFiles]
+      .filter(not(dirnameFilter(__dirname)))
       .map(filename => path.join(path.dirname(filename), 'README.md')),
   )
     .filter(filename => fs.existsSync(filename));
   if (submoduleReadmeFiles.length >= 1) {
     commands.push(
-      `node ./scripts/update-readme-badge.js ${submoduleReadmeFiles.join(' ')}`,
+      `node ${rootPath('scripts/update-readme-badge.js')} ${submoduleReadmeFiles.join(' ')}`,
       `git add ${submoduleReadmeFiles.join(' ')}`,
     );
   }
