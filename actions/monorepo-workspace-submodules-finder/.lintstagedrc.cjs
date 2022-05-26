3b2cceb3aa89b12b99a57d8eb587570690c0925a
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2022-05-26 22:53:50 +0900

    build(monorepo-workspace-submodules-finder-action): auto-build on commit should run after formatting

--- actions/monorepo-workspace-submodules-finder/.lintstagedrc.cjs
+++ actions/monorepo-workspace-submodules-finder/.lintstagedrc.cjs
@@ -6,7 +6,7 @@ const baseConfig = require('../../.lintstagedrc.cjs');
 
 /** @type {import('../../.lintstagedrc.cjs').LintStagedConfig} */
 module.exports = async filenames => [
+  ...(await baseConfig(filenames)),
   'pnpm build-with-cache',
   `git add ${path.resolve(__dirname, 'dist')}`,
-  ...(await baseConfig(filenames)),
 ];
