9dda1f1bca10427e472199f0c048a3196d97c655
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2022-06-17 16:56:12 +0900

    test(run-if-supported): enable ESM support for Jest and ts-jest
    see
    + https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
    + https://jestjs.io/docs/ecmascript-modules

--- packages/cli/run-if-supported/jest.config.cjs
+++ packages/cli/run-if-supported/jest.config.cjs
@@ -1,11 +1,18 @@
 module.exports = {
   ...require('../../../jest.config.base.cjs'),
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   coverageDirectory: 'coverage',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tests/tsconfig.json',
+      useESM: true,
     },
+  },
+  extensionsToTreatAsEsm: ['.mts', '.ts'],
+  moduleFileExtensions: ['js', 'mjs', 'cjs', 'ts', 'mts', 'cts', 'json', 'node'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\\.{1,2}/.*\\.[mc])js$': '$1ts',
   },
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.ts'],
@@ -13,4 +20,5 @@ module.exports = {
     '<rootDir>/tests/fixtures/',
     '<rootDir>/tests/helpers/',
   ],
+  transform: { '^.+\\.[mc]?ts$': 'ts-jest' },
 };
