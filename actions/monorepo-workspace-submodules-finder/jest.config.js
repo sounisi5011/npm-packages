ebd99e14409a7a2a1fc48692e7f27408ab623578
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2022-06-25 17:45:55 +0900

    test(monorepo-workspace-submodules-finder-action): migrate from `ts-jest` to `@swc/jest`

--- actions/monorepo-workspace-submodules-finder/jest.config.js
+++ actions/monorepo-workspace-submodules-finder/jest.config.js
@@ -1,12 +1,20 @@
-module.exports = {
-  ...require('../../jest.config.base.cjs'),
-  preset: 'ts-jest',
-  coverageDirectory: 'coverage',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tests/tsconfig.json',
+/**
+ * By default, @swc/jest@0.2.21 and @swc/core@1.2.203 does not seem to detect that .mts files are TypeScript.
+ * This behavior seems to be resolved by setting the compile config.
+ * @see https://swc.rs/docs/configuration/compilation
+ */
+const swcConfig = {
+  jsc: {
+    parser: {
+      syntax: 'typescript',
     },
   },
+};
+
+module.exports = {
+  ...require('../../jest.config.base.cjs'),
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.ts'],
+  transform: { '\\.[mc]?ts$': ['@swc/jest', swcConfig] },
 };
