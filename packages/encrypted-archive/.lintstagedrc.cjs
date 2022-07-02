02efdcccb1ab8b8780735e792bdf22d45fd25924
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2022-07-02 21:59:47 +0900

    build(@sounisi5011/encrypted-archive): do not execute `wireit` directly
    Ultra Runner executes the contents of npm-scripts directly.
    However, `wireit` reads the name of the script it is executing from and throws an error.
    To prevent this, the npm-scripts that run `wireit` should be called again with the `pnpm run` command.

--- packages/encrypted-archive/.lintstagedrc.cjs
+++ packages/encrypted-archive/.lintstagedrc.cjs
@@ -36,8 +36,11 @@ function endsWithFilter(...searchStringList) {
  * @type {import('../../.lintstagedrc.cjs').LintStagedConfig}
  */
 module.exports = async filenames => {
-  /** @type {string[]} */
-  const parallelScriptsList = [];
+  /** @type {{ src: boolean, test: boolean }} */
+  const protobufBuild = {
+    src: false,
+    test: false,
+  };
   /** @type {string[]} */
   const updatePathspecList = [];
 
@@ -46,7 +49,7 @@ module.exports = async filenames => {
       .filter(dirnameFilter([__dirname, 'src'], true))
       .some(endsWithFilter('.proto', '_pb.js', '_pb.d.ts'))
   ) {
-    parallelScriptsList.push('build-protobuf:src');
+    protobufBuild.src = true;
     updatePathspecList.push(
       ...['src/**/*.proto', 'src/**/*_pb.js', 'src/**/*_pb.d.ts'].map(p),
     );
@@ -56,7 +59,7 @@ module.exports = async filenames => {
       .filter(dirnameFilter([__dirname, 'tests/unit/fixtures']))
       .some(endsWithFilter('.prototxt', '.bin'))
   ) {
-    parallelScriptsList.push('build-protobuf:test');
+    protobufBuild.test = true;
     updatePathspecList.push(
       ...['tests/unit/fixtures/*.prototxt', 'tests/unit/fixtures/*.bin'].map(p),
     );
@@ -64,7 +67,13 @@ module.exports = async filenames => {
 
   /** @type {string[]} */
   const commands = [];
-  if (parallelScriptsList.length > 0) commands.push(`run-p ${parallelScriptsList.join(' ')}`);
+  if (protobufBuild.src && protobufBuild.test) {
+    commands.push('pnpm run ~build-protobuf');
+  } else if (protobufBuild.src) {
+    commands.push('pnpm run ~build-protobuf:src');
+  } else if (protobufBuild.test) {
+    commands.push('pnpm run ~build-protobuf:test');
+  }
   if (updatePathspecList.length > 0) commands.push(`git add ${updatePathspecList.join(' ')}`);
   return [...commands, ...(await baseConfig(filenames))];
 };
