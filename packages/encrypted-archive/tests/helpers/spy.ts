ec9d00ed13024d4dae6f2abdbf54fd6f3aa1aa8b
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2022-07-17 01:29:31 +0900

    test(@sounisi5011/encrypted-archive): check if the `StreamReader` class is doing unnecessary copying

---
+++ packages/encrypted-archive/tests/helpers/spy.ts
@@ -0,0 +1,159 @@
+import { inspect } from 'util';
+
+function isObject(value: unknown): value is object {
+    return (typeof value === 'object' || typeof value === 'function') && value !== null;
+}
+
+/**
+ * Check if a string is a valid ECMAScript 2015-2022 identifier name
+ * @see https://262.ecma-international.org/6.0/#sec-property-accessors
+ * @see https://262.ecma-international.org/6.0/#sec-names-and-keywords
+ * @see https://262.ecma-international.org/7.0/#sec-property-accessors
+ * @see https://262.ecma-international.org/7.0/#prod-IdentifierName
+ * @see https://262.ecma-international.org/8.0/#sec-property-accessors
+ * @see https://262.ecma-international.org/8.0/#prod-IdentifierName
+ * @see https://262.ecma-international.org/9.0/#sec-property-accessors
+ * @see https://262.ecma-international.org/9.0/#prod-IdentifierName
+ * @see https://262.ecma-international.org/9.0/#prod-IdentifierName
+ * @see https://262.ecma-international.org/10.0/#sec-property-accessors
+ * @see https://262.ecma-international.org/10.0/#prod-IdentifierName
+ * @see https://262.ecma-international.org/11.0/#sec-property-accessors
+ * @see https://262.ecma-international.org/11.0/#prod-IdentifierName
+ * @see https://262.ecma-international.org/12.0/#sec-property-accessors
+ * @see https://262.ecma-international.org/12.0/#prod-IdentifierName
+ * @see https://tc39.es/ecma262/2022/multipage/ecmascript-language-expressions.html#sec-property-accessors
+ * @see https://tc39.es/ecma262/2022/multipage/ecmascript-language-lexical-grammar.html#prod-IdentifierName
+ */
+function isValidIdentifierName(str: string): boolean {
+    return /^[\p{ID_Start}$_][\p{ID_Continue}$\u{200C}\u{200D}]*$/u.test(str);
+}
+
+function genPropAccessorCode(propName: string | symbol): string {
+    return typeof propName === 'string' && isValidIdentifierName(propName)
+        ? `.${propName}`
+        : `[${String(propName)}]`;
+}
+
+export interface SpyObjCallItem {
+    readonly type: keyof ProxyHandler<object>;
+    readonly path: string;
+}
+
+const origObj = new WeakMap<object, object>();
+const getOrig = <T>(value: T): T => {
+    if (!isObject(value)) return value;
+    return (origObj.get(value) as any) ?? value;
+};
+
+function internalSpyObj<T>(target: T, parentPath: string, callCallback: (callItem: SpyObjCallItem) => void): T {
+    if (!isObject(target)) return target;
+
+    const proxyObj = new Proxy(target, {
+        apply(target, thisArg, argArray) {
+            const path = `${parentPath}(${argArray.map(arg => inspect(arg, { breakLength: Infinity })).join(', ')})`;
+            callCallback({ type: 'apply', path });
+            return Reflect.apply(getOrig(target) as any, getOrig(thisArg), argArray);
+        },
+        construct(target, argArray, newTarget) {
+            const path = `new ${parentPath || '<self>'}(${
+                argArray.map(arg => inspect(arg, { breakLength: Infinity })).join(', ')
+            })`;
+            callCallback({ type: 'construct', path });
+            return internalSpyObj(
+                Reflect.construct(getOrig(target) as any, argArray, getOrig(newTarget) as any),
+                `(${path})`,
+                callCallback,
+            );
+        },
+        defineProperty(target, p, attributes) {
+            callCallback({ type: 'defineProperty', path: `${parentPath}${genPropAccessorCode(p)}` });
+            return Reflect.defineProperty(getOrig(target), p, attributes);
+        },
+        getOwnPropertyDescriptor(target, p) {
+            callCallback({ type: 'getOwnPropertyDescriptor', path: `${parentPath}${genPropAccessorCode(p)}` });
+            return Reflect.getOwnPropertyDescriptor(getOrig(target), p);
+        },
+        deleteProperty(target, p) {
+            callCallback({ type: 'deleteProperty', path: `${parentPath}${genPropAccessorCode(p)}` });
+            return Reflect.deleteProperty(getOrig(target), p);
+        },
+        get(target, p) {
+            const path = `${parentPath}${genPropAccessorCode(p)}`;
+            callCallback({ type: 'get', path });
+            return internalSpyObj(
+                Reflect.get(getOrig(target), p),
+                path,
+                callCallback,
+            );
+        },
+        has(target, p) {
+            callCallback({ type: 'has', path: `${parentPath}${genPropAccessorCode(p)}` });
+            return Reflect.has(getOrig(target), p);
+        },
+        set(target, p, value) {
+            callCallback({ type: 'set', path: `${parentPath}${genPropAccessorCode(p)}` });
+            return Reflect.set(getOrig(target), p, value);
+        },
+        getPrototypeOf(target) {
+            callCallback({ type: 'getPrototypeOf', path: `Object.getPrototypeOf(${parentPath || '<self>'})` });
+            // Note: If the return value of `getPrototypeOf()` is wrapped in a Proxy object,
+            //       the `instanceof` operator will not work.
+            return Reflect.getPrototypeOf(getOrig(target));
+        },
+        setPrototypeOf(target, v) {
+            callCallback({ type: 'setPrototypeOf', path: parentPath });
+            return Reflect.setPrototypeOf(getOrig(target), v);
+        },
+        ownKeys(target) {
+            callCallback({ type: 'ownKeys', path: parentPath });
+            return internalSpyObj(
+                Reflect.ownKeys(getOrig(target)),
+                parentPath,
+                callCallback,
+            );
+        },
+        isExtensible(target) {
+            callCallback({ type: 'isExtensible', path: parentPath });
+            return Reflect.isExtensible(getOrig(target));
+        },
+        preventExtensions(target) {
+            callCallback({ type: 'preventExtensions', path: parentPath });
+            return Reflect.preventExtensions(getOrig(target));
+        },
+    });
+    origObj.set(proxyObj, target);
+
+    return proxyObj;
+}
+
+export function spyObj<T extends object>(target: T, ignorePathList: ReadonlyArray<string | RegExp> = []): {
+    value: T;
+    calls: readonly SpyObjCallItem[];
+    newCalls: readonly SpyObjCallItem[];
+} {
+    const calls: SpyObjCallItem[] = [];
+    const value = internalSpyObj(target, '', item => {
+        if (
+            ignorePathList.some(ignorePath => {
+                if (ignorePath instanceof RegExp) {
+                    return ignorePath.test(item.path);
+                }
+                return item.path === ignorePath;
+            })
+        ) {
+            return;
+        }
+        calls.push(item);
+    });
+
+    let readedIndex = 0;
+    return {
+        value,
+        calls,
+        get newCalls() {
+            const ret = calls.slice(readedIndex);
+            readedIndex += calls.length;
+            return ret;
+        },
+    };
+}
--- packages/encrypted-archive/tests/unit/utils/stream.ts
+++ packages/encrypted-archive/tests/unit/utils/stream.ts
@@ -2,6 +2,9 @@ import * as stream from 'stream';
 
 import { StreamReader } from '../../../src/core/utils/stream';
 import { inspect } from '../../../src/runtimes/node/utils';
+import { spyObj, SpyObjCallItem } from '../../helpers/spy';
+
+/* eslint @typescript-eslint/no-unused-vars: ["error", { varsIgnorePattern: "^_+$" }] */
 
 const builtin: ConstructorParameters<typeof StreamReader>[0] = {
     inspect,
@@ -45,52 +48,120 @@ describe('class StreamReader', () => {
             });
         });
         it('multi read', async () => {
-            const chunkList = [
-                [0, 1, 2, 3],
-                [4],
-                [5, 6, 7, 8],
-            ];
+            const chunkSpyList = [
+                spyObj(Buffer.from([0, 1, 2, 3]), ['.then']),
+                spyObj(Buffer.from([4]), ['.then']),
+                spyObj(Buffer.from([5, 6, 7, 8]), ['.then']),
+            ] as const;
 
             let readCount = 0;
-            const targetStream = stream.Readable.from(chunkList.map(chunkData => Buffer.from(chunkData)))
+            const targetStream = stream.Readable.from(chunkSpyList.map(({ value }) => value))
                 .on('data', () => {
                     readCount++;
                 });
             const reader = new StreamReader(builtin, targetStream);
 
             expect(readCount).toBe(0);
+            // StreamReader should not touch any chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
 
             await expect(reader.read(0)).resolves
                 .toBytesEqual(Buffer.from([]));
             expect(readCount).toBe(0);
+            // StreamReader should not touch any chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
 
             await expect(reader.read(1)).resolves
                 .toBytesEqual(Buffer.from([0]));
             expect(readCount).toBe(1);
+            {
+                const newCalls0 = chunkSpyList[0].newCalls;
+                // StreamReader should not copy chunks
+                expect(newCalls0).not.toIncludeAnyMembers<SpyObjCallItem>([
+                    { type: 'get', path: '[0]' },
+                    { type: 'get', path: '[1]' },
+                    { type: 'get', path: '[2]' },
+                    { type: 'get', path: '[3]' },
+                    { type: 'get', path: '.buffer' },
+                ]);
+                expect(newCalls0).toIncludeAllMembers<SpyObjCallItem>([
+                    { type: 'apply', path: '.subarray(0, 1)' },
+                ]);
+                // StreamReader should not touch other chunks
+                expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+                expect(chunkSpyList[2].newCalls).toStrictEqual([]);
+            }
 
             await expect(reader.read(4)).resolves
                 .toBytesEqual(Buffer.from([0, 1, 2, 3]));
             expect(readCount).toBe(1);
+            expect(chunkSpyList[0].newCalls).toIncludeAllMembers<SpyObjCallItem>([
+                { type: 'apply', path: '.subarray(0, 4)' },
+            ]);
+            // StreamReader should not touch other chunks
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
 
             await expect(reader.read(5)).resolves
                 .toBytesEqual(Buffer.from([0, 1, 2, 3, 4]));
             expect(readCount).toBe(2);
+            // StreamReader should copy chunks
+            expect(chunkSpyList[0].newCalls).toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '[1]' },
+                { type: 'get', path: '[2]' },
+                { type: 'get', path: '[3]' },
+                { type: 'get', path: '.buffer' },
+            ]);
+            expect(chunkSpyList[1].newCalls).toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '.buffer' },
+            ]);
+            // StreamReader should not touch other chunks
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
 
             await expect(reader.read(4)).resolves
                 .toBytesEqual(Buffer.from([0, 1, 2, 3]));
             expect(readCount).toBe(2);
+            // StreamReader should not touch any chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
 
             await expect(reader.read(2, 3)).resolves
                 .toBytesEqual(Buffer.from([3, 4]));
             expect(readCount).toBe(2);
+            // StreamReader should not touch any chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
 
             await expect(reader.read(2, 4)).resolves
                 .toBytesEqual(Buffer.from([4, 5]));
             expect(readCount).toBe(3);
+            // StreamReader should not touch other chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            // StreamReader should copy chunks
+            expect(chunkSpyList[2].newCalls).toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '[1]' },
+                { type: 'get', path: '[2]' },
+                { type: 'get', path: '[3]' },
+                { type: 'get', path: '.buffer' },
+            ]);
 
             await expect(reader.read(10, 4)).resolves
                 .toBytesEqual(Buffer.from([4, 5, 6, 7, 8]));
             expect(readCount).toBe(3);
+            // StreamReader should not touch any chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
         });
     });
     describe('readIterator() method', () => {
@@ -297,6 +368,73 @@ describe('class StreamReader', () => {
                 ]);
             }
         });
+        it('should not copy chunks', async () => {
+            const chunkSpyList = [
+                spyObj(Buffer.from([9, 8, 7]), ['.then']),
+                spyObj(Buffer.from([6, 5]), ['.then']),
+                spyObj(Buffer.from([4]), ['.then']),
+                spyObj(Buffer.from([3, 2, 1]), ['.then']),
+                spyObj(Buffer.from([0]), ['.then']),
+            ] as const;
+            const reader = new StreamReader(builtin, chunkSpyList.map(({ value }) => value));
+
+            // StreamReader should not touch any chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[3].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[4].newCalls).toStrictEqual([]);
+
+            for await (const _ of reader.readIterator(3));
+            // StreamReader should not copy chunks
+            expect(chunkSpyList[0].newCalls).not.toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '[1]' },
+                { type: 'get', path: '[2]' },
+                { type: 'get', path: '.buffer' },
+            ]);
+            // StreamReader should not touch other chunks
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[3].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[4].newCalls).toStrictEqual([]);
+
+            for await (const _ of reader.readIterator(3));
+            // StreamReader should not copy chunks
+            expect(chunkSpyList[1].newCalls).not.toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '[1]' },
+                { type: 'get', path: '.buffer' },
+            ]);
+            expect(chunkSpyList[2].newCalls).not.toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '.buffer' },
+            ]);
+            // StreamReader should not touch other chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[3].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[4].newCalls).toStrictEqual([]);
+
+            await reader.read(1, 3);
+            chunkSpyList.forEach(({ newCalls }) => newCalls);
+
+            for await (const _ of reader.readIterator(99));
+            // StreamReader should not copy chunks
+            expect(chunkSpyList[3].newCalls).not.toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '[1]' },
+                { type: 'get', path: '[2]' },
+                { type: 'get', path: '.buffer' },
+            ]);
+            expect(chunkSpyList[4].newCalls).not.toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '.buffer' },
+            ]);
+            // StreamReader should not touch other chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
+        });
     });
     describe('seek() method', () => {
         describe.each([
@@ -336,6 +474,69 @@ describe('class StreamReader', () => {
                 });
             });
         });
+        it('should not copy chunks', async () => {
+            const chunkSpyList = [
+                spyObj(Buffer.from([9, 8, 7, 6, 5]), ['.then']),
+                spyObj(Buffer.from([4, 3]), ['.then']),
+                spyObj(Buffer.from([2, 1, 0]), ['.then']),
+            ] as const;
+            const reader = new StreamReader(builtin, chunkSpyList.map(({ value }) => value));
+
+            // StreamReader should not touch any chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[2].newCalls).toStrictEqual([]);
+
+            await reader.seek(3);
+            {
+                const newCalls0 = chunkSpyList[0].newCalls;
+                // StreamReader should not copy chunks
+                expect(newCalls0).not.toIncludeAnyMembers<SpyObjCallItem>([
+                    { type: 'get', path: '[0]' },
+                    { type: 'get', path: '[1]' },
+                    { type: 'get', path: '[2]' },
+                    { type: 'get', path: '[3]' },
+                    { type: 'get', path: '[4]' },
+                    { type: 'get', path: '.buffer' },
+                ]);
+                expect(newCalls0).toIncludeAllMembers<SpyObjCallItem>([
+                    { type: 'apply', path: '.subarray(3)' },
+                ]);
+                // StreamReader should not touch other chunks
+                expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+                expect(chunkSpyList[2].newCalls).toStrictEqual([]);
+            }
+
+            await reader.seek(3);
+            {
+                const newCalls1 = chunkSpyList[1].newCalls;
+                // StreamReader should not copy chunks
+                expect(newCalls1).not.toIncludeAnyMembers<SpyObjCallItem>([
+                    { type: 'get', path: '[0]' },
+                    { type: 'get', path: '[1]' },
+                    { type: 'get', path: '.buffer' },
+                ]);
+                expect(newCalls1).toIncludeAllMembers<SpyObjCallItem>([
+                    { type: 'apply', path: '.subarray(1)' },
+                ]);
+                // StreamReader should not touch other chunks
+                expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+                expect(chunkSpyList[2].newCalls).toStrictEqual([]);
+            }
+
+            await reader.seek(999);
+            // StreamReader should not copy chunks
+            expect(chunkSpyList[2].newCalls).not.toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '[1]' },
+                { type: 'get', path: '[2]' },
+                { type: 'get', path: '.buffer' },
+                { type: 'get', path: '.subarray' },
+            ]);
+            // StreamReader should not touch other chunks
+            expect(chunkSpyList[0].newCalls).toStrictEqual([]);
+            expect(chunkSpyList[1].newCalls).toStrictEqual([]);
+        });
     });
     describe('isEnd() method', () => {
         it('empty stream', async () => {
@@ -355,6 +556,23 @@ describe('class StreamReader', () => {
 
             await reader.seek(1);
             await expect(reader.isEnd()).resolves.toBeTrue();
+        });
+        it('should not copy chunks', async () => {
+            const chunkSpy = spyObj(Buffer.from([4, 2, 3, 7, 1, 9]), ['.then']);
+            const reader = new StreamReader(builtin, [chunkSpy.value]);
+
+            // StreamReader should not touch any chunks
+            expect(chunkSpy.newCalls).toStrictEqual([]);
+
+            await expect(reader.isEnd()).resolves.toBeFalse();
+            // StreamReader should not copy chunks
+            expect(chunkSpy.newCalls).not.toIncludeAnyMembers<SpyObjCallItem>([
+                { type: 'get', path: '[0]' },
+                { type: 'get', path: '[1]' },
+                { type: 'get', path: '[2]' },
+                { type: 'get', path: '.buffer' },
+                { type: 'get', path: '.subarray' },
+            ]);
         });
     });
 });
