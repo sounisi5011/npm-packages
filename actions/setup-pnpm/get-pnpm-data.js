204e0e77393959602a7f1d9a70b4036878e13df5
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-01-02 00:07:10 +0900

    ci(./actions/setup-pnpm): should not use the deprecated `set-output` workflow command
    see https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/

--- actions/setup-pnpm/get-pnpm-data.js
+++ actions/setup-pnpm/get-pnpm-data.js
@@ -1,5 +1,48 @@
+// @ts-check
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+/**
+ * @param {string} name
+ * @param {string} value
+ * @returns {void}
+ * @see https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
+ */
+function setOutput(name, value) {
+  // eslint-disable-next-line dot-notation
+  const outputFilepath = process.env['GITHUB_OUTPUT'];
+  if (outputFilepath) {
+    /** @type {number|null} */
+    let fd = null;
+    try {
+      /**
+       * Open file for appending. An exception occurs if the file does not exist.
+       * @see https://github.com/nodejs/node/issues/1592#issuecomment-223819785
+       */
+      fd = fs.openSync(outputFilepath, fs.constants.O_APPEND | fs.constants.O_WRONLY);
+      /**
+       * Note: With the `flag` option, there is no need to use the `fs.openSync()` function in the above line.
+       *       However, it is not documented that the `flag` option can be passed a number.
+       *       We use the `fs.openSync()` function to ensure that it will work in future Node.js.
+       */
+      fs.writeFileSync(fd, `${name}=${value}${os.EOL}`);
+      return;
+    } finally {
+      if (typeof fd === 'number') {
+        fs.closeSync(fd);
+      }
+    }
+  }
+
+  /**
+   * If writing to the "GITHUB_OUTPUT" file fails, try the old approach.
+   */
+  console.log(`::set-output name=${name}::${value}`);
+}
+
 function main() {
-  const path = require('path');
   const pkgJsonPath = path.resolve('package.json');
   const pkg = require(pkgJsonPath);
   console.log(`Loaded ${pkgJsonPath}`);
@@ -13,7 +56,7 @@ function main() {
   if (typeof pnpmVersionRange !== 'string') throw new Error('`engines.pnpm` field is not string value');
 
   console.log(`Detect pnpm version: ${pnpmVersionRange}`);
-  console.log(`::set-output name=pnpm-version-range::${pnpmVersionRange || ''}`);
+  setOutput('pnpm-version-range', pnpmVersionRange || '');
 }
 
 try {
