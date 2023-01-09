3f9187171f296b99e3a778942d5a83d99fa347dd
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-01-09 12:42:49 +0900

    style(dprint): automatic formatting of all JS / TS files

--- packages/encrypted-archive/src/core/header/protocol-buffers-converter/utils.ts
+++ packages/encrypted-archive/src/core/header/protocol-buffers-converter/utils.ts
@@ -55,65 +55,62 @@ export function validateNumberOptionInRange<T extends number | bigint>(
     return value;
 }
 
-export function createEnum2value<TValue>(): (
-    <TEnumKey extends string, TEnum>(enumRecord: Record<TEnumKey, TEnum>) => (
-        <TEnum2 extends TEnum, TValue2 extends TValue>(
-            /**
-             * Type checking asserts that all enum combinations are specified
-             * @see https://stackoverflow.com/a/60132060/4907315
-             */
-            pair:
-                & OneOrMoreReadonlyArray<readonly [TEnum2, TValue2]>
-                & Cond2<{
-                    cond1: {
-                        actual: TEnum2;
-                        expected: TEnum;
-                        onlyMatch: 'Invalid: All value types must be specified';
-                    };
-                    cond2: {
-                        actual: TValue2;
-                        expected: TValue;
-                        onlyMatch: 'Invalid: All Enum type values must be specified';
-                    };
-                    allMatch: unknown;
-                    notAllMatch: 'Invalid: All Enum type values and all value types must be specified';
-                }>,
-        ) => {
-            enum2value: (
-                builtin: BuiltinInspectRecord,
-                enumItem: Nullable<TEnum2>,
-                exists: boolean,
-                opts: { fieldName: string; dataName: string },
-            ) => TValue2;
-            value2enum: (builtin: BuiltinInspectRecord, value: TValue2) => TEnum2;
-        }
-    )
-) {
-    return enumRecord =>
-        pair => {
-            const enum2valueMap = new Map(pair.map(([e, v]) => [e, { data: v }]));
-            const value2enumMap = new Map(pair.map(([e, v]) => [v, { data: e }]));
-
-            type TEnum2 = (typeof enum2valueMap) extends Map<infer U, unknown> ? U : never;
-            const isEnumType = enum2valueMap.has.bind(enum2valueMap) as (value: unknown) => value is TEnum2;
-
-            return {
-                enum2value: (builtin, enumItem, exists, { fieldName, dataName }) => {
-                    if (!exists || (!isEnumType(enumItem) && (enumItem === undefined || enumItem === null))) {
-                        reportNonDefinedField({ fieldName, dataName });
-                    }
-                    const value = enum2valueMap.get(enumItem);
-                    if (value) return value.data;
-                    throw new Error(
-                        `The value in the ${fieldName} field in the ${dataName} is unknown.`
-                            + ` Received: ${getPropFromValue(enumRecord, enumItem) ?? builtin.inspect(enumItem)}`,
-                    );
-                },
-                value2enum: (builtin, value) => {
-                    const enumItem = value2enumMap.get(value);
-                    if (enumItem) return enumItem.data;
-                    throw new Error(`Unknown Argon2 algorithm received: ${builtin.inspect(value)}`);
-                },
+export function createEnum2value<TValue>(): <TEnumKey extends string, TEnum>(
+    enumRecord: Record<TEnumKey, TEnum>,
+) => <TEnum2 extends TEnum, TValue2 extends TValue>(
+    /**
+     * Type checking asserts that all enum combinations are specified
+     * @see https://stackoverflow.com/a/60132060/4907315
+     */
+    pair:
+        & OneOrMoreReadonlyArray<readonly [TEnum2, TValue2]>
+        & Cond2<{
+            cond1: {
+                actual: TEnum2;
+                expected: TEnum;
+                onlyMatch: 'Invalid: All value types must be specified';
             };
+            cond2: {
+                actual: TValue2;
+                expected: TValue;
+                onlyMatch: 'Invalid: All Enum type values must be specified';
+            };
+            allMatch: unknown;
+            notAllMatch: 'Invalid: All Enum type values and all value types must be specified';
+        }>,
+) => {
+    enum2value: (
+        builtin: BuiltinInspectRecord,
+        enumItem: Nullable<TEnum2>,
+        exists: boolean,
+        opts: { fieldName: string; dataName: string },
+    ) => TValue2;
+    value2enum: (builtin: BuiltinInspectRecord, value: TValue2) => TEnum2;
+} {
+    return enumRecord => pair => {
+        const enum2valueMap = new Map(pair.map(([e, v]) => [e, { data: v }]));
+        const value2enumMap = new Map(pair.map(([e, v]) => [v, { data: e }]));
+
+        type TEnum2 = (typeof enum2valueMap) extends Map<infer U, unknown> ? U : never;
+        const isEnumType = enum2valueMap.has.bind(enum2valueMap) as (value: unknown) => value is TEnum2;
+
+        return {
+            enum2value: (builtin, enumItem, exists, { fieldName, dataName }) => {
+                if (!exists || (!isEnumType(enumItem) && (enumItem === undefined || enumItem === null))) {
+                    reportNonDefinedField({ fieldName, dataName });
+                }
+                const value = enum2valueMap.get(enumItem);
+                if (value) return value.data;
+                throw new Error(
+                    `The value in the ${fieldName} field in the ${dataName} is unknown.`
+                        + ` Received: ${getPropFromValue(enumRecord, enumItem) ?? builtin.inspect(enumItem)}`,
+                );
+            },
+            value2enum: (builtin, value) => {
+                const enumItem = value2enumMap.get(value);
+                if (enumItem) return enumItem.data;
+                throw new Error(`Unknown Argon2 algorithm received: ${builtin.inspect(value)}`);
+            },
         };
+    };
 }
