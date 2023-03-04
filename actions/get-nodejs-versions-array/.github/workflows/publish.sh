e3add5acaa7c5cc0ecb53c5f79cb89787adecbe1
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-03-04 20:29:36 +0900

    ci(./actions/get-nodejs-versions-array): create a release tag containing only actions directly under the repository at release time
    Until now, users had to use long action names like this:
    
    ```yaml
    steps:
      - uses: sounisi5011/npm-packages/actions/get-nodejs-versions-array@get-nodejs-versions-array-action-v0
    ```
    
    However, by creating this release tag, users can now use short action names like this:
    
    ```yaml
    steps:
      - uses: sounisi5011/npm-packages@actions/get-nodejs-versions-array-v0
    ```

--- actions/get-nodejs-versions-array/.github/workflows/publish.sh
+++ actions/get-nodejs-versions-array/.github/workflows/publish.sh
@@ -15,3 +15,95 @@ update_git_tag() {
 
 update_git_tag "${matrix_package_name_without_scope}-v${outputs_major}"                  "Release ${matrix_package_name_with_scope}@${outputs_major}.x"
 update_git_tag "${matrix_package_name_without_scope}-v${outputs_major}.${outputs_minor}" "Release ${matrix_package_name_with_scope}@${outputs_major}.${outputs_minor}.x"
+
+# Try to create a commit having an action directly under the repository.
+# This allows users to use this action without having to specify a directory name.
+GIT_ROOT_PATH="$(git rev-parse --show-toplevel)"
+GIT_RELEASE_COMMIT_REF="$(git rev-parse HEAD)"
+PKG_ROOT_DIRNAME="$(realpath --relative-to="${GIT_ROOT_PATH}" "${PWD}")"
+readonly GIT_ROOT_PATH GIT_RELEASE_COMMIT_REF PKG_ROOT_DIRNAME
+readonly GITHUB_URL_PREFIX='https://github.com/sounisi5011/npm-packages'
+readonly TAG_NAME_PREFIX='actions/get-nodejs-versions-array'
+readonly latestTagName="${TAG_NAME_PREFIX}-latest"
+
+cd "${GIT_ROOT_PATH}"
+echo '::group::$' git checkout "refs/tags/${latestTagName}"
+if git checkout "refs/tags/${latestTagName}"; then
+  echo '::endgroup::'
+
+  echo '::group::$' git merge --no-commit "${GIT_RELEASE_COMMIT_REF}"
+  git merge --no-commit "${GIT_RELEASE_COMMIT_REF}" || true
+  echo '::endgroup::'
+else
+  echo '::endgroup::'
+  echo '[!] This action has not yet been published'
+  # shellcheck disable=SC2154
+  git switch -c "action-only--${outputs_tag_name}"
+fi
+
+echo '::group::Delete everything except the ".git" directory'
+find "${GIT_ROOT_PATH}" -maxdepth 1 -mindepth 1 -type d -name '.git' -prune -o -print0 | xargs -0 rm -rf
+ls -lhpa
+echo '::endgroup::'
+
+echo '::group::Restore only files required for custom actions'
+git restore --source="${GIT_RELEASE_COMMIT_REF}" "${PKG_ROOT_DIRNAME}"/{action.yaml,dist}
+mv "${GIT_ROOT_PATH}/${PKG_ROOT_DIRNAME}"/* "${GIT_ROOT_PATH}/"
+ls -lhpa
+echo '::endgroup::'
+
+echo '::group::Create README.md file'
+{
+  echo '> **Note**'
+  echo '> This place is a copy of [`'"${outputs_tag_name}"'` tag].'
+  # shellcheck disable=SC2016
+  echo '> If you want to contribute, please move to [the `main` branch]!'
+  echo
+  # shellcheck disable=SC2016
+  echo '[the `main` branch]:' "${GITHUB_URL_PREFIX}/tree/main/${PKG_ROOT_DIRNAME}"
+  echo '[`'"${outputs_tag_name}"'` tag]:' "${GITHUB_URL_PREFIX}/tree/${outputs_tag_name}/${PKG_ROOT_DIRNAME}"
+} > "${GIT_ROOT_PATH}/README.md"
+cat "${GIT_ROOT_PATH}/README.md"
+echo '::endgroup::'
+
+echo '::group::Create CHANGELOG.md file'
+{
+  echo "Please see ${GITHUB_URL_PREFIX}/blob/${outputs_tag_name}/${PKG_ROOT_DIRNAME}/CHANGELOG.md"
+} > "${GIT_ROOT_PATH}/CHANGELOG.md"
+cat "${GIT_ROOT_PATH}/CHANGELOG.md"
+echo '::endgroup::'
+
+echo '::group::Create LICENSE file'
+licenseFilename="${PKG_ROOT_DIRNAME}/LICENSE"
+# see https://stackoverflow.com/a/444317
+# see https://stackoverflow.com/a/4709925
+if git ls-tree -r --name-only --full-name "${GIT_RELEASE_COMMIT_REF}" | grep -qxF "${licenseFilename}"; then
+  :
+elif git ls-tree --name-only --full-name "${GIT_RELEASE_COMMIT_REF}" | grep -qxF 'LICENSE'; then
+  licenseFilename='LICENSE'
+else
+  licenseFilename=''
+fi
+if [ -z "${licenseFilename}" ]; then
+  echo "::warning::LICENSE file not found in release commit ( ${GIT_RELEASE_COMMIT_REF} )"
+else
+  {
+    echo "Please see ${GITHUB_URL_PREFIX}/blob/${outputs_tag_name}/${licenseFilename}"
+  } > "${GIT_ROOT_PATH}/LICENSE"
+  cat "${GIT_ROOT_PATH}/LICENSE"
+fi
+echo '::endgroup::'
+
+echo '::group::Add commit'
+git add --all
+git commit --reuse-message="${GIT_RELEASE_COMMIT_REF}" --no-verify
+echo '::endgroup::'
+
+echo '::group::Add Git Tags'
+# shellcheck disable=SC2154
+readonly tagMessage="Release ${matrix_package_name_with_scope}@${outputs_major}.${outputs_minor}.${outputs_patch}"
+update_git_tag "${latestTagName}" "${tagMessage}"
+update_git_tag "${TAG_NAME_PREFIX}-v${outputs_major}" "${tagMessage}"
+update_git_tag "${TAG_NAME_PREFIX}-v${outputs_major}.${outputs_minor}" "${tagMessage}"
+update_git_tag "${TAG_NAME_PREFIX}-v${outputs_major}.${outputs_minor}.${outputs_patch}" "${tagMessage}"
+echo '::endgroup::'
